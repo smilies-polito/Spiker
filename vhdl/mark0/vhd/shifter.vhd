774f747d22f06cd3ac7c2c858bd559b0cc6484f4
alessio <alecarpa96@gmail.com>
2021-11-03 18:04:46 +0100

    Primi componenti HW dell'architettura.

---
+++ vhdl/mark0/vhd/shifter.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity shifter is
+
+	port(
+				
+	);
+
+end entity shifter;
+
+
+architecture behaviour of shifter is
+begin
+
+end architecture behaviour;
