04f91033706e08c3d33373934deec54272b791bb
alessio <alecarpa96@gmail.com>
2021-11-04 09:51:05 +0100

    Rimuovo i file di simulazione e aggiungo lo shifter.

--- vhdl/mark0/vhd/shifter.vhd
+++ vhdl/mark0/vhd/shifter.vhd
@@ -5,8 +5,20 @@ use ieee.numeric_std.all;
 
 entity shifter is
 
+	generic(
+		-- parallelism
+		N		: integer := 8;
+		
+		-- shift
+		shift		: integer := 1	
+	);
+
 	port(
-				
+		-- input
+		shifter_in	: in signed(N-1 downto 0);
+
+		-- output
+		shifted_out	: out signed(N-1 downto 0)
 	);
 
 end entity shifter;
@@ -14,5 +26,9 @@ end entity shifter;
 
 architecture behaviour of shifter is
 begin
+
+	shifted_out(N-1 downto N-1-shift)	<= (others => shifter_in(N-1));
+
+	shifted_out(N-1-shift downto 0)		<= shifter_in(N-1 downto shift);	
 
 end architecture behaviour;
