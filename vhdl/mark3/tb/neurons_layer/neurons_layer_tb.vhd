676e400950f578390e02eb5f7fcc714c7e4c39b2
alessio <alecarpa96@gmail.com>
2022-01-20 17:38:06 +0100

    Aggiorno testbench strato di neuroni.

--- vhdl/mark3/tb/neurons_layer/neurons_layer_tb.vhd
+++ vhdl/mark3/tb/neurons_layer/neurons_layer_tb.vhd
@@ -12,6 +12,7 @@ architecture behaviour of neurons_layer_tb is
 
 	-- parallelism
 	constant N		: integer := 16;
+	constant N_weights	: integer := 15;
 
 	-- number of neurons in the layer
 	constant layer_size	: integer := 4;
@@ -20,21 +21,19 @@ architecture behaviour of neurons_layer_tb is
 	constant shift		: integer := 10;
 
 	-- model parameters
-	constant v_th_0_int	: integer 	:= 13*(2**10);	
+	constant v_th_value_int	: integer 	:= 13*(2**10);	
 	constant v_reset_int	: integer 	:= 5*(2**10);	
-	constant v_th_plus_int	: integer	:= 102; -- 0.1*2^11 rounded	
 	constant inh_weight_int	: integer 	:= 7*(2**10);	
 	constant exc_weight_int	: integer 	:= 7*(2**10);
 
 
 	-- input parameters
-	signal v_th_0		: signed(N-1 downto 0);
+	signal v_th_value	: signed(N-1 downto 0);
 	signal v_reset		: signed(N-1 downto 0);
-	signal v_th_plus	: signed(N-1 downto 0);
 	signal inh_weight	: signed(N-1 downto 0);
-	signal exc_weights	: signed(layer_size*N-1 downto 0);
+	signal exc_weights	: signed(layer_size*N_weights-1 downto 0);
 
-	-- input
+	-- control input
 	signal clk		: std_logic;
 	signal rst_n		: std_logic;
 	signal start		: std_logic;
@@ -43,15 +42,17 @@ architecture behaviour of neurons_layer_tb is
 	signal exc_stop		: std_logic;
 	signal inh_or		: std_logic;
 	signal inh_stop		: std_logic;
+	
+	-- to load the threshold
+	signal v_th_en		: std_logic_vector(layer_size-1 downto 0);
+
+	-- input
         signal input_spikes	: std_logic_vector(layer_size-1 downto 0);
 
 
 	-- output
 	signal out_spikes	: std_logic_vector(layer_size-1 downto 0);
-	signal neurons_ready	: std_logic;
-
-
-
+	signal all_ready	: std_logic;
 
 
 
@@ -59,10 +60,11 @@ architecture behaviour of neurons_layer_tb is
 
 		generic(
 			-- internal parallelism
-			N		: integer := 8;
+			N		: integer := 16;
+			N_weights	: integer := 5;
 
 			-- number of neurons in the layer
-			layer_size	: integer := 3;
+			layer_size	: integer := 400;
 
 			-- shift during the exponential decay
 			shift		: integer := 1
@@ -78,46 +80,45 @@ architecture behaviour of neurons_layer_tb is
 			exc_stop	: in std_logic;
 			inh_or		: in std_logic;
 			inh_stop	: in std_logic;
-			input_spikes	: in std_logic_vector(layer_size-1
-						  downto 0);
+			v_th_en		: in std_logic_vector(layer_size-1 downto 0);
+
+			-- input
+			input_spikes	: in std_logic_vector(layer_size-1 downto 0);
 
 			-- input parameters
-			v_th_0		: in signed(N-1 downto 0);		
+			v_th_value	: in signed(N-1 downto 0);		
 			v_reset		: in signed(N-1 downto 0);		
 			inh_weight	: in signed(N-1 downto 0);		
-			v_th_plus	: in signed(N-1 downto 0);		
-			exc_weights	: in signed(layer_size*N-1 downto 0);
+			exc_weights	: in signed(layer_size*N_weights-1 downto 0);
 
 			-- output
-			out_spikes	: out std_logic_vector(layer_size-1 
-						downto 0);
-			neurons_ready	: out std_logic
+			out_spikes	: out std_logic_vector(layer_size-1 downto 0);
+			all_ready	: out std_logic
 		);
 
 	end component neurons_layer;
+
+
 
 begin
 
 
 	-- model parameters binary conversion
-	v_th_0		<= to_signed(v_th_0_int, N);
+	v_th_value	<= to_signed(v_th_value_int, N);
 	v_reset		<= to_signed(v_reset_int, N);
-	v_th_plus	<= to_signed(v_th_plus_int, N);
 	inh_weight	<= to_signed(inh_weight_int, N);
 
 	exc_weights_init	: process
 	begin
 		init	: for i in 0 to layer_size-1
 		loop
-			exc_weights((i+1)*N-1 downto i*N) <= 
-					to_signed(exc_weight_int, N);
+			exc_weights((i+1)*N_weights-1 downto i*N_weights) <= 
+					to_signed(exc_weight_int, N_weights);
 		end loop init;
 
 		wait;
 
 	end process exc_weights_init;
-
-
 
 
 
@@ -142,6 +143,20 @@ begin
 		rst_n	<= '1';		-- 17 ns
 		wait;
 	end process reset_gen;
+
+
+
+	-- v_th_en
+	v_th_en_gen : process
+	begin
+		v_th_en	<= (others => '0');		-- 0 ns
+		wait for 26 ns;
+		v_th_en	<= (others => '1');		-- 26 ns
+		wait for 12 ns;
+		v_th_en	<= (others => '0');		-- 38 ns
+		wait;
+	end process v_th_en_gen;
+
 
 
 
@@ -225,22 +240,22 @@ begin
 	input_spikes_gen: process
 	begin
 		input_spikes	<= (others => '0');	-- 0 ns	
-		wait for 62 ns;
-		input_spikes	<= (others => '1'); -- 62 ns
-		wait for 96 ns;
-		input_spikes	<= (others => '0'); -- 158 ns
-		wait for 24 ns;
-		input_spikes	<= (others => '1'); -- 182 ns
-		wait for 12 ns;
-		input_spikes	<= (others => '0'); -- 194 ns
-		wait for 12 ns;
-		input_spikes	<= (others => '1'); -- 206 ns
-		wait for 12 ns;
-		input_spikes	<= (others => '0'); -- 218 ns
-		wait for 12 ns;
-		input_spikes	<= (others => '1'); -- 230 ns
-		wait for 12 ns;
-		input_spikes	<= (others => '0'); -- 242 ns
+		wait for 62 ns;	          
+		input_spikes	<= (others => '1');	-- 62 ns
+		wait for 96 ns;					  
+		input_spikes	<= (others => '0');	-- 158 ns
+		wait for 24 ns;				      
+		input_spikes	<= (others => '1');	-- 182 ns
+		wait for 12 ns;				      
+		input_spikes	<= (others => '0');	-- 194 ns
+		wait for 12 ns;				      
+		input_spikes	<= (others => '1');	-- 206 ns
+		wait for 12 ns;				      
+		input_spikes	<= (others => '0');	-- 218 ns
+		wait for 12 ns;				      
+		input_spikes	<= (others => '1');	-- 230 ns
+		wait for 12 ns;				      
+		input_spikes	<= (others => '0');	-- 242 ns
 		wait;
 	end process input_spikes_gen;
 
@@ -261,14 +276,12 @@ begin
 
 
 
-
-
-
 	dut : neurons_layer
 
 		generic map(
 			-- parallelism
 			N		=> N,	
+			N_weights	=> N_weights,
 
 			-- number of neurons in the layer
 			layer_size	=> layer_size,
@@ -287,18 +300,22 @@ begin
 			exc_stop       	=> exc_stop,
 			inh_or	        => inh_or,
 			inh_stop        => inh_stop,
+
+			-- to load the threshold
+			v_th_en		=> v_th_en,
+
+			-- input
                        	input_spikes	=> input_spikes,
 
 			-- input parameters
-			v_th_0		=> v_th_0,
+			v_th_value	=> v_th_value,
 			v_reset		=> v_reset,
 			inh_weight	=> inh_weight,
 			exc_weights	=> exc_weights,
-			v_th_plus	=> v_th_plus,
                                                        
 			-- output          
 			out_spikes	=> out_spikes,
-			neurons_ready	=> neurons_ready
+			all_ready	=> all_ready
 		);
 
 
