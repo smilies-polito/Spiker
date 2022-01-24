8cc4e5c5dd45402f3a239d118cfd3983afa487c6
alessio <alecarpa96@gmail.com>
2022-01-24 18:38:32 +0100

    Testbench strato. C'è un errore nel design.
    Al momento sembra che gli impulsi inibitori non vengano in alcun modo
    mascherati.

--- vhdl/mark3/tb/layer/layer_cu_dp_tb.vhd
+++ vhdl/mark3/tb/layer/layer_cu_dp_tb.vhd
@@ -10,26 +10,32 @@ architecture test of layer_cu_dp_tb is
 
 
 	-- int parallelism
-	constant parallelism		: integer := 16;
-	constant weightsParallelism	: integer := 5;
+	constant parallelism		: integer := 32;
+	constant weightsParallelism	: integer := 31;
 
 	-- input spikes
-	constant N_inputs		: integer := 784;
+	constant N_inputs		: integer := 2;
 
 	-- must be one bit larger that the parallelism required to count
 	-- up to N_inputs
-	constant N_inputs_cnt		: integer := 11;
+	constant N_inputs_cnt		: integer := 2;
 
 	-- inhibitory spikes
-	constant N_neurons		: integer := 400;
+	constant N_neurons		: integer := 3;
 
 	-- must be one bit larger that the parallelism required to count
 	-- up to N_neurons
-	constant N_neurons_cnt		: integer := 10;
+	constant N_neurons_cnt		: integer := 3;
 
 	-- exponential decay shift
 	constant shift			: integer := 10;
 
+	-- model parameters
+	constant v_th_value_int	: integer 	:= 13*(2**10);	
+	constant v_reset_int	: integer 	:= 5*(2**10);	
+	constant v_th_plus_int	: integer	:= 102; -- 0.1*2^11 rounded	
+	constant inh_weight_int	: integer 	:= 5*(2**10);	
+	constant exc_weight_int	: integer 	:= 7*(2**10);
 
 	-- control input
 	signal clk			: std_logic;
@@ -260,6 +266,120 @@ begin
 	inh_or		<= inh_or_int;
 	inh_stop	<= inh_stop_int;
 
+
+	N_neurons_tc	<= "011";
+	N_inputs_tc	<= "10";
+
+
+	-- model parameters binary conversion
+	v_th_value	<= to_signed(v_th_value_int, parallelism);
+	v_reset		<= to_signed(v_reset_int, parallelism);
+	inh_weight	<= to_signed(inh_weight_int, parallelism);
+
+	exc_weights_init	: process
+	begin
+		init	: for i in 0 to N_neurons-1
+		loop
+			exc_weights((i+1)*weightsParallelism-1 downto 
+					i*weightsParallelism) <= 
+					to_signed(exc_weight_int, 
+					weightsParallelism);
+		end loop init;
+
+		wait;
+
+	end process exc_weights_init;
+
+
+
+	-- clock
+	clock_gen : process
+	begin
+		clk	<= '0';		-- falling edge i*12ns
+		wait for 6 ns;			                    
+		clk	<= '1';         -- rising edge 6ns + i*12ns
+		wait for 6 ns;			
+	end process clock_gen;
+
+
+
+	-- reset
+	reset_gen : process
+	begin
+		rst_n	<= '1';		-- 0 ns
+		wait for 14 ns;
+		rst_n	<= '0';		-- 14 ns
+		wait for 3 ns;
+		rst_n	<= '1';		-- 17 ns
+		wait;
+	end process reset_gen;
+
+
+	-- load_v_th
+	init_v_th_gen	: process
+	begin
+
+		init_v_th	<= '0';	-- 0ns
+		wait for 26 ns;
+		init_v_th	<= '1';	-- 26 ns
+		wait for 48 ns;
+		init_v_th	<= '0';	-- 74 ns
+		wait;
+
+	end process init_v_th_gen;
+
+
+	-- v_th_addr
+	v_th_addr_gen : process
+	begin
+		wait for 26 ns;
+		
+		for i in 0 to N_neurons-1
+		loop
+
+			v_th_addr <= std_logic_vector(to_unsigned(i,
+						N_neurons_cnt));
+			wait for 12 ns;
+
+		end loop;
+
+		-- wait for 156 ns;
+
+		-- v_th_addr	<= "11";	-- 230 ns
+
+		wait;
+	end process v_th_addr_gen;
+
+
+
+
+	-- start
+	start_gen : process
+	begin
+		start	<= '0';		-- 0 ns
+		wait for 158 ns;
+		start	<= '1';		-- 158 ns
+		wait for 12 ns;
+		start	<= '0';		-- 170 ns
+		wait;
+	end process start_gen;
+
+
+
+	-- input_spikes
+	input_spikes_gen: process
+	begin
+		input_spikes	<= "00"; -- 0 ns	
+		wait for 182 ns;
+		input_spikes	<= "11"; -- 182 ns
+		wait for 48 ns;
+		input_spikes	<= "00"; -- 230 ns
+		wait for 48 ns;
+		input_spikes	<= "01"; -- 258 ns
+		wait for 84 ns;
+		input_spikes	<= "00"; -- 342 ns
+		wait;
+	end process input_spikes_gen;
 
 
 
