634655240d322b65b0850c57df21eb0d533b7c1f
alessio.carpegna <alecarpa96@gmail.com>
2022-02-15 15:51:04 +0100

    File di inizializzazione dei parametri.
    Salvo i nomi dei file e i parametri della conversione dei pesi nel formato
    giusto per la BRAM in file dedicati.

---
+++ vhdl/mark3/scripts/files.py
@@ -0,0 +1,6 @@
+hyperparametersDir = "../hyperparameters"
+
+weightsFilename = "../../../python/custom/mark3_simplified/"\
+			"parameters/weights1.npy"
+
+bramRootFilename = hyperparametersDir + "/weights"
---
+++ vhdl/mark3/scripts/parameters.py
@@ -0,0 +1,19 @@
+wordWidthsList = [
+	72,
+	36,
+	18,
+	9,
+	4,
+	2,
+	1
+]
+
+bramSize = 36e3 # 36Kb
+
+numberOfNeurons = 400
+
+numberOfInputs = 784
+
+bitWidth = 5
+
+fixed_point_decimals = 3
