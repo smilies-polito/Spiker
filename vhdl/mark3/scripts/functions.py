696342adbcc83a0e5268b4eb4ec9c087277339f7
alessio.carpegna <alecarpa96@gmail.com>
2022-02-15 15:49:56 +0100

    Creazione cartella. Fixed point. Piccole modifiche.
    Aggiungo funzioni di creazione cartella e conversione in fixed point copiandole
    da python/custom/mark4_finitePrecision/utils.py.

--- vhdl/mark3/scripts/functions.py
+++ vhdl/mark3/scripts/functions.py
@@ -1,3 +1,31 @@
+import subprocess as sp
+
+def createDir(dirName):
+
+	'''
+	Create a new directory. If it already exists it is firstly remove.
+
+	INPUT:
+
+		dirName: string. Name of the directory to create
+	'''
+
+	# Check if the directory exists
+	cmdString = "if [[ -d " + dirName + " ]]; then "
+
+	# If it exists remove it
+	cmdString += "rm -r " + dirName + "; "
+	cmdString += "fi; "
+
+	# Create the directory
+	cmdString += "mkdir " + dirName + "; "
+	
+	# Run the complete bash command
+	sp.run(cmdString, shell=True, executable="/bin/bash")
+
+
+
+
 def storeWeights(weightsArray, subArraySize, bitWidth, wordWidth, rootFilename):
 
 	subArraysN = int(weightsArray.shape[0]//subArraySize)
@@ -8,14 +36,14 @@ def storeWeights(weightsArray, subArraySize, bitWidth, wordWidth, rootFilename):
 		filename = rootFilename + str(i) + ".mem"
 
 		formatAndStore(weightsArray[i*subArraySize :
-			(i+1)*subArraySize], bitWidth, wordWidth, filename)
+			(i+1)*subArraySize].T, bitWidth, wordWidth, filename)
 
 	if lastArraySize > 0:
 
 		filename = rootFilename + str(subArraysN)  + ".mem"	
 
 		formatAndStore(weightsArray[subArraysN*subArraySize :
-			subArraysN*subArraySize + lastArraySize], bitWidth,
+			subArraysN*subArraySize + lastArraySize].T, bitWidth,
 			wordWidth, filename)
 
 
@@ -61,3 +89,23 @@ def storeList(inputList, filename):
 
 			fp.write(element)
 			fp.write("\n")
+
+
+
+
+def fixedPointArray(numpyArray, fixed_point_decimals):
+
+	'''
+	Convert a NumPy array into fixed point notation.
+
+	INPUT:
+
+		1) numpyArray: floating point array to convert.
+
+		2) fixed_point_decimals: number of decimal bits in the fixed
+		point representation.
+
+	'''
+
+	numpyArray = numpyArray * 2**fixed_point_decimals
+	return numpyArray.astype(int)
