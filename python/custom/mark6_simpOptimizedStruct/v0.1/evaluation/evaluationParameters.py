b101f11127edfb340bcd1d548edc563b643cdea2
alessio <alessio.carpegna@polito.it>
2022-02-22 15:26:51 +0000

    Allineo v0.0 a mark3 con parametri layer diversi.

---
+++ python/custom/mark6_simpOptimizedStruct/v0.1/evaluation/evaluationParameters.py
@@ -0,0 +1,34 @@
+import sys
+
+import numpy as np
+from parameters import *
+
+
+
+N_sim = 1500
+
+density = 0.005
+
+networkList = [3, 2]
+
+mode = "train"
+
+excDictList = [excDict] * (len(networkList) - 1)
+
+inh2excWeights = inh2excWeight * np.ones(len(networkList) - 1)
+ 
+
+scaleFactors = np.array([10])
+
+dt = 1
+
+dt_tauDict = {
+
+	"exc" 		: dt/tauExc,
+	"inh"		: dt/tauExc,
+	"thresh"	: dt/tauThresh
+
+}
+
+stdpDict["ltp_dt_tau"] = dt/stdpDict["ltp_tau"]
+stdpDict["ltd_dt_tau"] = dt/stdpDict["ltd_tau"]
---
+++ python/custom/mark6_simpOptimizedStruct/v0.1/parameters.py
@@ -0,0 +1,37 @@
+# Excitatory layer
+excDict = {
+
+	"vRest"		: -65.0,	# mV
+	"vReset"	: -60.0,	# mV
+	"vThresh0"	: -52.0,	# mV
+	"vThreshPlus"	: 0.05,		# mV
+}
+
+# STDP synapses
+stdpParam = {
+
+	# Learning rates
+	"eta_pre"	: 1e-4,		# mV
+	"eta_post"	: 1e-3,		# mV
+
+	# Time constants
+	"ltp_tau"	: 20,		# ms
+	"ltd_tau"	: 20		# ms
+}
+
+# Inhibitory synapses
+inh2excWeight = -15	           	# mV
+
+# Membrane potential time constant
+tauExc = 100				# ms
+tauThresh = 1e7				# ms
+
+# Weights normalization factors
+constSum = 1000 #78.4
+
+# Scaling factor for the random generation of the weights
+scaleFactor = 0.5 #0.3
+
+# Reference sizes used for constSum and scaleFactor
+refInLayerSize = 784
+refCurrLayerSize = 400
