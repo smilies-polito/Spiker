3d95b4a2ce2bc5a298ef43a72c40601b20223be3
alessio <alessio.carpegna@polito.it>
2022-06-01 10:28:18 +0200

    Compilazione automatica dei componenti di debug.

---
+++ vhdl/mark3/sim/debug_compile.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+VHD_DIR="../vhd"
+DEBUG_DIR="../debug"
+TB_DIR="../tb"
+
+TOP_ENTITY="debug_layer"
+
+# Neuron
+xvhdl $VHD_DIR"/neuron/add_sub.vhd"
+xvhdl $VHD_DIR"/neuron/cmp_gt.vhd"
+xvhdl $VHD_DIR"/neuron/mux2to1_signed.vhd"
+xvhdl $VHD_DIR"/neuron/mux4to1_signed.vhd"
+xvhdl $DEBUG_DIR"/neuron/debug_neuron.vhd"
+xvhdl $VHD_DIR"/neuron/neuron_cu.vhd"
+xvhdl $DEBUG_DIR"/neuron/debug_neuron_datapath.vhd"
+xvhdl $VHD_DIR"/neuron/reg_signed_sync_rst.vhd"
+xvhdl $VHD_DIR"/neuron/shifter.vhd"
+
+# Bare neurons
+xvhdl $DEBUG_DIR"/bare_neurons/debug_bare_neurons.vhd"
+xvhdl $VHD_DIR"/bare_neurons/generic_and.vhd"
+
+# Complete neurons
+xvhdl $DEBUG_DIR"/complete_neurons/debug_complete_neurons.vhd"
+xvhdl $VHD_DIR"/complete_neurons/double_and_mask_n.vhd"
+xvhdl $VHD_DIR"/complete_neurons/simple_and_mask.vhd"
+
+# Layer
+xvhdl $VHD_DIR"/layer/anticipate_bits.vhd"
+xvhdl $VHD_DIR"/layer/bit_selection.vhd"
+xvhdl $VHD_DIR"/layer/cmp_eq.vhd"
+xvhdl $VHD_DIR"/layer/cnt.vhd"
+xvhdl $VHD_DIR"/layer/generic_mux_1bit.vhd"
+xvhdl $VHD_DIR"/layer/generic_or.vhd"
+xvhdl $DEBUG_DIR"/layer/debug_layer.vhd"
+xvhdl $VHD_DIR"/layer/layer_cu.vhd"
+xvhdl $DEBUG_DIR"/layer/debug_layer_datapath.vhd"
+xvhdl $VHD_DIR"/layer/mux2to1.vhd"
+xvhdl $VHD_DIR"/layer/reg.vhd"
+
+# Shared components
+xvhdl $VHD_DIR"/shared/decoder.vhd"
+xvhdl $VHD_DIR"/shared/mux2to1_std_logic.vhd"
+xvhdl $VHD_DIR"/shared/reg_signed.vhd"
