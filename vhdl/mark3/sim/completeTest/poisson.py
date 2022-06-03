ee129ac5b1cdcd1c40e57e3a51045eefb7a75f4c
alessio <alessio.carpegna@polito.it>
2022-06-03 22:53:32 +0200

    Elimino funzione di importazione impulsi.

--- vhdl/mark3/sim/completeTest/poisson.py
+++ vhdl/mark3/sim/completeTest/poisson.py
@@ -3,49 +3,6 @@
 import numpy as np
 
 
-def importSpikes(filename, trainingSteps, inputLayerSize):
-
-	'''
-	Read the input spikes from file and convert them into a numpy array.
-
-	INPUT:
-		1) filename: string. Name of the file containing the spikes in text
-		form.
-
-		2) trainingSteps: total amount of time steps associated to the
-		pixels' spikes trains. This must be equal to the number of lines
-		in the file.
-
-		3) inputLayerSize: total amount of input spikes per each cycle.
-		Each line of the file must contain exactly this amount of "0"
-		and "1".
-	'''
-
-	spikesTrains = np.zeros((trainingSteps, inputLayerSize)).astype(bool)
-	i = 0
-
-	# Read one single string from the input file
-	with open(filename, "r") as fp:
-
-		for line in fp:
-
-			# Remove newline character
-			binaryString = line[:-1]
-
-			# Convert string into list
-			binaryList = list(binaryString)
-
-			# Convert list of characters into array of integers
-			binaryArray = np.array(binaryList).astype(int)
-
-			# Convert integers to boolean
-			spikesTrains[i] = binaryArray.astype(bool)
-			
-			i += 1
-
-	return spikesTrains
-	
-
 def imgToSpikeTrain(image, dt, trainingSteps, inputIntensity, rng):
 
 	''' 
