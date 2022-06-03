61494fc9f27fe41b73432f172d5450a6a7c4e889
alessio <alessio.carpegna@polito.it>
2022-06-03 16:30:50 +0200

    Generazione impulsi in cartella dedicata.

---
+++ vhdl/mark3/sim/inputGen/files.py
@@ -0,0 +1,11 @@
+# Directpory containing the approximated python simulation of the model
+pythonSrcDir = "../../../../python/custom/mark4_finitePrecision"
+
+
+# Complete path of the MNIST files
+trainImages = "../../../../mnist/train-images-idx3-ubyte"
+trainLabels = "../../../../mnist/train-labels-idx1-ubyte"
+testImages = "../../../../mnist/t10k-images-idx3-ubyte"
+testLabels = "../../../../mnist/t10k-labels-idx1-ubyte"
+
+inSpikesFilename = "../inputOutput/inputSpikes.txt"
