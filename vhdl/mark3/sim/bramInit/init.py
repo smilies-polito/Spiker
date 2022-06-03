19f8428e51e61db90aba1e08dea73cce4d011327
alessio <alessio.carpegna@polito.it>
2022-06-03 14:40:14 +0200

    Funzione dedicata a inizializzazione BRAM.
    Trasformo quello che prima era uno script a sè stante in una funzione in modo da
    poterla chiamare nella simulazione completa.

--- vhdl/mark3/sim/bramInit/init.py
+++ vhdl/mark3/sim/bramInit/init.py
@@ -4,59 +4,62 @@ from functions import storeWeights, fixedPointArray, createDir, formatAndStore
 from files import *
 from parameters import *
 
-# Create the target directory in which the weights will be stored
-createDir(hyperparametersDir)
+
+def bramInit(hyperparametersDir, weightsFilename, inThreshFilename,
+		wordWidthsList, bramSize, numberOfInputs, weightsBitWidth,
+		neuronsBitWidth, bramRootFilename, outThreshFilename):
+
+	# Create the target directory in which the weights will be stored
+	createDir(hyperparametersDir)
+
+	# Import the weights in form of a numpy array
+	with open(weightsFilename, 'rb') as fp:
+		weightsArray = np.load(fp)
+
+	# Import the thresholds in form of a numpy array
+	with open(inThreshFilename, 'rb') as fp:
+		threshArray = np.load(fp)
 
 
+	# Quantize the weights to fixed point
+	weightsArray = fixedPointArray(weightsArray, fixed_point_decimals)
 
-# Import the weights in form of a numpy array
-with open(weightsFilename, 'rb') as fp:
-	weightsArray = np.load(fp)
-
-# Import the thresholds in form of a numpy array
-with open(inThreshFilename, 'rb') as fp:
-	threshArray = np.load(fp)
+	# Quantize the thresholds to fixed point
+	threshArray = fixedPointArray(threshArray, fixed_point_decimals)
 
 
-# Quantize the weights to fixed point
-weightsArray = fixedPointArray(weightsArray, fixed_point_decimals)
+	i = 0
+	flag = 0
 
-# Quantize the thresholds to fixed point
-threshArray = fixedPointArray(threshArray, fixed_point_decimals)
+	# Find the proper word-width for the BRAM between the available ones
+	while i < len(wordWidthsList) and flag == 0:
 
+		# Check if all the inputs can be stored with the selected
+		# word-width
+		if int(bramSize//wordWidthsList[i]) > numberOfInputs:
 
-i = 0
-flag = 0
+			# Set the corresponding word-width and exit
+			wordWidth = wordWidthsList[i]
+			flag = 1
 
-# Find the proper word-width for the BRAM between the available ones
-while i < len(wordWidthsList) and flag == 0:
+		i += 1
 
-	# Check if all the inputs can be stored with the selected word-width
-	if int(bramSize//wordWidthsList[i]) > numberOfInputs:
+	# Set the number of neurons that can be stored within the BRAM
+	subArraySize = int(wordWidth//weightsBitWidth)
 
-		# Set the corresponding word-width and exit
-		wordWidth = wordWidthsList[i]
-		flag = 1
+	# Store the weights in binary format to import them to initialize the
+	# BRAMs
+	storeWeights(weightsArray, subArraySize, weightsBitWidth, wordWidth,
+			bramRootFilename, ".mem")
+	with open(outThreshFilename, "w") as fp:
 
-	i += 1
+		for threshold in threshArray[0]:
 
-# Set the number of neurons that can be stored within the BRAM
-subArraySize = int(wordWidth//weightsBitWidth)
+			binaryValue = binaryFormat.format(threshold)
 
-# Store the weights in binary format to import them to initialize the BRAMs
-storeWeights(weightsArray, subArraySize, weightsBitWidth, wordWidth,
-		bramRootFilename, ".mem")
+			fp.write(binaryValue)
+			fp.write("\n")
 
-
-
-
-binaryFormat = "{0:0" + str(neuronsBitWidth) + "b}"
-
-with open(outThreshFilename, "w") as fp:
-
-	for threshold in threshArray[0]:
-
-		binaryValue = binaryFormat.format(threshold)
-
-		fp.write(binaryValue)
-		fp.write("\n")
+bramInit(hyperparametersDir, weightsFilename, inThreshFilename,
+		wordWidthsList, bramSize, numberOfInputs, weightsBitWidth,
+		neuronsBitWidth, bramRootFilename, outThreshFilename)
