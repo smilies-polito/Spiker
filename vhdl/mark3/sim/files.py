14e6db0290556300e09d7a5fa6d231ef19478946
alessio <alessio.carpegna@polito.it>
2022-06-03 14:54:32 +0200

    Unico script con tutti i nomi dei file richiesti.

---
+++ vhdl/mark3/sim/files.py
@@ -0,0 +1,15 @@
+pythonSrcDir = "../../../python/custom/mark4_finitePrecision"
+bramInitDIr = "bramInit"
+
+
+hyperparametersDir = "hyperparameters"
+
+weightsFilename = "../../../python/custom/mark3_simplified/"\
+			"parameters/weights1.npy"
+
+inThreshFilename = "../../../python/custom/mark3_simplified/"\
+			"parameters/thresholds1.npy"
+
+bramRootFilename = hyperparametersDir + "/weights"
+
+outThreshFilename = hyperparametersDir + "/thresholds.init"
