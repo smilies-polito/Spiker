407a03b4d60842d87ab9ceb5e662e17d9df4924a
alessio <alessio.carpegna@polito.it>
2022-06-03 16:28:25 +0200

    Prove scriptone python unico. Fallimentare.

--- vhdl/mark3/sim/automaticVhdlTest.py
+++ vhdl/mark3/sim/automaticVhdlTest.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from files import *
+from parameters import *
 
 if pythonSrcDir not in sys.path:
 	sys.path.insert(1, pythonSrcDir)
@@ -21,26 +22,25 @@ if bramInitDir not in sys.path:
 from bramInit import bramInit
 
 
-
-
-
 # Load the MNIST dataset
 imgArray, labelsArray = loadDataset(testImages, testLabels)
 
 
 # Create the network data structure
-network = createNetwork(networkList, weightFilename, thresholdFilename, mode, 
+network = createNetwork(networkList, weightsFilename, inThreshFilename, mode, 
 			excDictList, scaleFactors, inh2excWeights,
 			fixed_point_decimals, trainPrecision, rng)
 
 checkParallelism(network["exc2exc1"]["weights"], weights_parallelism)
 
+# Create the binary files to initialize the bram
+bramInit(hyperparametersDir, weightsFilename, inThreshFilename,
+		wordWidthsList, bramSize, numberOfInputs, weightsBitWidth,
+		neuronsBitWidth, bramRootFilename, outThreshFilename)
 
 
 
-
-
-currentIndex = int(sys.argv[1])
+currentIndex = 0
 
 
 # Measure the test starting time
@@ -87,65 +87,65 @@ with open(membraneFilename, "w") as membrane_fp:
 storePerformace(startTimeTraining, accuracies, testPerformanceFile)
 
 
-
-
-
-
-N_out = 16
-N_neurons = 400
-
-testImages = "../../../../mnist/t10k-images-idx3-ubyte"
-testLabels = "../../../../mnist/t10k-labels-idx1-ubyte"
-
-inputsFilename = "inputs.txt"
-outputsFilename = "cntOut.txt"
-
-
-sp.run("make clean", shell=True)
-
-sp.run("./compile.sh")
-
-# Load the MNIST dataset
-imgArray, labelsArray = loadDataset(testImages, testLabels)
-
-for image in imgArray:
-
-	# Convert the image into spikes trains
-	spikesTrains = imgToSpikeTrain(image, dt, testSteps, inputIntensity,
-			rng)
-
-	with open(inputsFilename, "w") as fp:
-
-		for step in spikesTrains:
-
-			inputs = np.array2string(step.astype(int),
-					max_line_width = step.shape[0]*2+2)
-
-			inputs = inputs[1:-1]
-
-			inputs = inputs.replace(" ", "")
-
-			fp.write(inputs)
-			fp.write("\n")
-
-
-	sp.run("./sim.sh")
-
-	with open(outputsFilename) as fp:
-
-		countersString = fp.readline()
-
-		counters = [0 for _ in range(N_neurons)]
-
-		for i in range(N_neurons):
-
-			binaryString = countersString[i*N_out:(i+1)*N_out]
-
-			counters[i] = int(binaryString, 2)
-
-	print(counters)
-
-	break
-
-
-sp.run("make clean", shell=True)
+#
+#
+#
+#
+#N_out = 16
+#N_neurons = 400
+#
+#testImages = "../../../../mnist/t10k-images-idx3-ubyte"
+#testLabels = "../../../../mnist/t10k-labels-idx1-ubyte"
+#
+#inputsFilename = "inputs.txt"
+#outputsFilename = "cntOut.txt"
+#
+#
+#sp.run("make clean", shell=True)
+#
+#sp.run("./compile.sh")
+#
+## Load the MNIST dataset
+#imgArray, labelsArray = loadDataset(testImages, testLabels)
+#
+#for image in imgArray:
+#
+#	# Convert the image into spikes trains
+#	spikesTrains = imgToSpikeTrain(image, dt, testSteps, inputIntensity,
+#			rng)
+#
+#	with open(inputsFilename, "w") as fp:
+#
+#		for step in spikesTrains:
+#
+#			inputs = np.array2string(step.astype(int),
+#					max_line_width = step.shape[0]*2+2)
+#
+#			inputs = inputs[1:-1]
+#
+#			inputs = inputs.replace(" ", "")
+#
+#			fp.write(inputs)
+#			fp.write("\n")
+#
+#
+#	sp.run("./sim.sh")
+#
+#	with open(outputsFilename) as fp:
+#
+#		countersString = fp.readline()
+#
+#		counters = [0 for _ in range(N_neurons)]
+#
+#		for i in range(N_neurons):
+#
+#			binaryString = countersString[i*N_out:(i+1)*N_out]
+#
+#			counters[i] = int(binaryString, 2)
+#
+#	print(counters)
+#
+#	break
+#
+#
+#sp.run("make clean", shell=True)
