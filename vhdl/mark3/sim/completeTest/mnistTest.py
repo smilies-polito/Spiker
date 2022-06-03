41548709da61b514c6211db85fb047f7f2767ecd
alessio <alessio.carpegna@polito.it>
2022-06-03 21:46:41 +0200

    Rimuovo creazione rete e reintroduco loop di test.

--- vhdl/mark3/sim/completeTest/mnistTest.py
+++ vhdl/mark3/sim/completeTest/mnistTest.py
@@ -20,58 +20,47 @@ from runParameters import *
 
 
 # Load the MNIST dataset
-_ , labelsArray = loadDataset(testImages, testLabels)
+imgArray , labelsArray = loadDataset(testImages, testLabels)
 
 
-# Create the network data structure
-network = createNetwork(networkList, weightFilename, thresholdFilename, mode, 
-			excDictList, scaleFactors, inh2excWeights,
-			fixed_point_decimals, trainPrecision, rng)
-
-checkParallelism(network["exc2exc1"]["weights"], weights_parallelism)
-
-currentIndex = int(sys.argv[1])
+currentIndex = 0
+numberOfCycles = imgArray.shape[0]
 
 
 # Measure the test starting time
 startTimeTraining = timeit.default_timer()
 
-# Complete test cycle over a single image
-inputIntensity, currentIndex, accuracies, spikesMonitor, membraneMonitor = \
-	singleImageTest(
-		trainDuration,
-		restTime,
-		dt,
-		None,
-		network,
-		networkList,
-		dt_tauDict,
-		countThreshold,
-		inputIntensity,
-		currentIndex,
-		spikesEvolution,
-		updateInterval,
-		printInterval,
-		startTimeTraining,
-		accuracies,
-		labelsArray,
-		assignments,
-		startInputIntensity,
-		mode,
-		constSums,
-		rng,
-		exp_shift,
-		neuron_parallelism,
-		inputFilename
-	)
 
-with open(outSpikesFilename, "w") as spikes_fp:
-	spikes_fp.write(str(list(spikesMonitor.astype(int))).replace(",",
-		"").replace(" ", "\n")[1:-1])
-
-with open(membraneFilename, "w") as membrane_fp:
-	membrane_fp.write(str(list(membraneMonitor.astype(int))).replace(",",
-		"").replace(" ", "\n")[1:-1])
+while currentIndex < numberOfCycles:
+	# Complete test cycle over a single image
+	inputIntensity, currentIndex, accuracies, spikesMonitor, \
+	membraneMonitor = \
+		singleImageTest(
+			trainDuration,
+			restTime,
+			dt,
+			imgArray[currentIndex],
+			network,
+			networkList,
+			dt_tauDict,
+			countThreshold,
+			inputIntensity,
+			currentIndex,
+			spikesEvolution,
+			updateInterval,
+			printInterval,
+			startTimeTraining,
+			accuracies,
+			labelsArray,
+			assignments,
+			startInputIntensity,
+			mode,
+			constSums,
+			rng,
+			exp_shift,
+			neuron_parallelism,
+			inputFilename
+		)
 
 # Store the performance of the network into a text file
 storePerformace(startTimeTraining, accuracies, testPerformanceFile)
