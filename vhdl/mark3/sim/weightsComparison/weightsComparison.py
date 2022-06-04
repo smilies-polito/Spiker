88c3d3476a6cee8140330940b0484d91ce88274d
alessio <alessio.carpegna@polito.it>
2022-06-04 11:49:59 +0200

    Confronto pesi BRAM e python.

---
+++ vhdl/mark3/sim/weightsComparison/weightsComparison.py
@@ -0,0 +1,46 @@
+import subprocess as sp
+import numpy as np
+
+from createNetwork import createNetwork
+from parameters import *
+from runParameters import *
+
+with open("../inputOutput/outWeights.txt") as fp:
+	
+	vhdlWeights = np.zeros((788, 400)).astype(int)
+	samples = np.zeros(788).astype(int)
+	exc_cnt = np.zeros(788).astype(int)
+
+	i=0
+
+	for line in fp:
+
+		sample, exc_cnt_string, weights_string = line.split()
+
+		samples[i] = int(sample)
+
+		exc_cnt[i] = int(exc_cnt_string, 2)
+
+		for j in range(400):
+
+			vhdlWeights[i][j] = int(weights_string[j*5:(j+1)*5], 2)
+
+		i += 1
+
+
+
+
+# Create the network data structure
+network = createNetwork(networkList, weightFilename, thresholdFilename, mode, 
+			excDictList, scaleFactors, inh2excWeights,
+			fixed_point_decimals, trainPrecision, rng)
+
+pythonWeights = network["exc2exc1"]["weights"]
+
+for i in range(400):
+
+	print(pythonWeights[i])
+	print(vhdlWeights.T[400-1-i][1:785])
+
+	for j in range(100000000):
+		a = j
