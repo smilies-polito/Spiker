03d499359454bcbf2c546c5da72edbffc4428f35
alessio.carpegna <alecarpa96@gmail.com>
2022-06-08 14:17:28 +0200

    Elimino mark5 e mark6 python.
    Mark5 l'avevo creata per dare dati a Sepide. Mark6 sincero non mi ricordo. Sono
    lì inutilizzate da mesi. Le elimino, male che vada le vengo a riprendere se
    serve.

--- python/custom/mark5_dataCollection/parallelisms.py
+++
@@ -1,4 +0,0 @@
-weights_parallelism	= 12
-neuron_parallelism	= 31
-fixed_point_decimals	= 9
-exp_shift		= 10
--- python/custom/mark6_simpOptimizedStruct/v2/evaluation/plot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python layersEvaluation.py
-
-open -a Safari excSpikes.svg
-open -a Safari inhSpikes.svg
-open -a Safari v.svg
-open -a Safari outSpikes.svg
