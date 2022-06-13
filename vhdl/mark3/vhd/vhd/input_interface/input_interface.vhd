6a7695234b2b49d016159371d96aec0c158c248c
alessio <alessio.carpegna@polito.it>
2022-06-13 16:43:36 +0200

    Scheletro vuoto interfaccia di ingresso VHDL.

---
+++ vhdl/mark3/vhd/vhd/input_interface/input_interface.vhd
@@ -0,0 +1,11 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity input_interface is
+
+end entity input_interface;
+
+architecture behaviour of input_interface is
+begin
+
+end architecture behaviour;
