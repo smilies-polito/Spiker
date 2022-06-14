9d273f70af8c075ec3a24e25ffb65accb2b2dd7f
alessio <alessio.carpegna@polito.it>
2022-06-14 22:20:59 +0200

    Testbench per macchina a stati interfaccia uscita.

--- vhdl/mark3/tb/vhd/output_interface/out_interface_cu_tb.vhd
+++ vhdl/mark3/tb/vhd/output_interface/out_interface_cu_tb.vhd
@@ -37,6 +37,55 @@ architecture behaviour of out_interface_cu_tb is
 
 begin
 
+	-- clock
+	clk_gen		: process
+	begin
+		clk <= '0';
+		wait for 10 ns;
+		clk <= '1';
+		wait for 10 ns;
+	end process clk_gen;
+
+	-- start
+	start_gen	: process
+	begin
+		start <= '0';
+		wait for 52 ns;
+		start <= '1';
+		wait for 20 ns;
+		start <= '0';
+		wait for 100 ns;
+		start <= '1';
+		wait for 20 ns;
+		start <= '0';
+		wait;
+	end process start_gen;
+
+	-- stop
+	stop_gen	: process
+	begin
+		stop <= '0';
+		wait for 132 ns;
+		stop <= '1';
+		wait for 20 ns;
+		stop <= '0';
+		wait;
+	end process stop_gen;
+
+
+	-- reset
+	rst_n_gen	: process
+	begin
+		rst_n <= '1';
+		wait for 12 ns;
+		rst_n <= '0';
+		wait for 5 ns;
+		rst_n <= '1';
+		wait;
+	end process rst_n_gen;
+
+
+
 	dut	: out_interface_cu 
 
 		port map(
