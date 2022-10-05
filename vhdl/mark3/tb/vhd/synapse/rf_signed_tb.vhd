31ced2221990bb4fc4a959f6273415e2afecda2a
alessio <alessio.carpegna@polito.it>
2022-10-05 14:10:04 +0200

    Testbench refister file signed. Scheletro.

---
+++ vhdl/mark3/tb/vhd/synapse/rf_signed_tb.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity rf_signed_tb is
+end entity rf_signed_tb;
+
+architecture test of rf_signed_tb is
+
+begin
+
+end architecture test;
