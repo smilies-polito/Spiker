141936abc828c16f0288a9f6e3b86a6d70188100
alessio <alessio.carpegna@polito.it>
2023-01-09 17:54:37 +0100

    Comment the file names.

--- Python/Brian2/PeterDiehl/files.py
+++ Python/Brian2/PeterDiehl/files.py
@@ -1,7 +1,14 @@
 mnistDir = "../../../MNIST"
+
+# Training
 images = mnistDir + "/train-images-idx3-ubyte"
 labels = mnistDir + "/train-labels-idx1-ubyte"
 
+# Test
+images = mnistDir + "/t10k-images-idx3-ubyte"
+labels = mnistDir + "/t10k-labels-idx1-ubyte"
+
+# Where to store the trained hyper-parameters and the time/accuracy results
 paramDir = "./parameters"
 
 weightFilename = paramDir + "/weights"
