29d491b4a4eb017fb72b82f6a3decb1a2741f80b
alessio <alessio.carpegna@polito.it>
2023-01-09 18:08:03 +0100

    Comment the equation selection function.

--- Python/Brian2/PeterDiehl/equations/equations.py
+++ Python/Brian2/PeterDiehl/equations/equations.py
@@ -3,6 +3,18 @@ from testEquations import *
 
 def defineEquations(mode):
 
+	"""
+	Define the network equations based on the execution mode.
+
+	INPUT:
+		mode: string. Can be "train" or "test"
+
+	OUTPUT:
+		Dictionaries containing the equation for the neuron model and
+		the stdp
+		
+	"""
+
 	if mode == "train":
 		return trainEquationsDict, trainStdpDict
 
