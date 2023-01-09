8cc372188a2980ae4759cfa775c87d8ecd207fc2
alessio <alessio.carpegna@polito.it>
2023-01-09 17:31:03 +0100

    Add missing import in store functions.

--- Python/Brian2/PeterDiehl/storeParameters.py
+++ Python/Brian2/PeterDiehl/storeParameters.py
@@ -1,5 +1,6 @@
 import numpy as np
 from utils import seconds2hhmmss
+import timeit
 
 def storeParameters(networkList, network, assignements, weightFilename,
 			thetaFilename, assignementsFilename):
