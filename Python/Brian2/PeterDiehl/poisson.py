c6e16f2dac1ade860bc6ab160d2acdda1b0acc1f
alessio <alessio.carpegna@polito.it>
2023-01-09 17:57:31 +0100

    Comment input Poisson function.

--- Python/Brian2/PeterDiehl/poisson.py
+++ Python/Brian2/PeterDiehl/poisson.py
@@ -1,6 +1,21 @@
 import brian2 as b2
 
 def imgToSpikeTrain(network, image, inputIntensity):
+
+	"""
+	Associate the inputs of the Poisson layer to the values of the input
+	image.
+
+	INPUT:
+
+		1) network: Brian 2 Network object.
+
+		2) image: Numpy array of float values.
+
+		3) inputIntensity: float. Variable normalization factor. If the
+		spiking activity is too low the input intensity can be
+		increased.
+	"""
 	
 	values = {
 		"poissongroup":{
@@ -9,4 +24,3 @@ def imgToSpikeTrain(network, image, inputIntensity):
 	}
 
 	network.set_states(values, units=True, format='dict', level=0)
-
