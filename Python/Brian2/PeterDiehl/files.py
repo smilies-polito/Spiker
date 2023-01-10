9fc3e35ea8b61d20981c0a5dd73100a5448b06d7
alessio <alessio.carpegna@polito.it>
2023-01-10 09:46:20 +0100

    Comment test dataset files.
    The training dataset files were overwritten by the test ones.

--- Python/Brian2/PeterDiehl/files.py
+++ Python/Brian2/PeterDiehl/files.py
@@ -5,8 +5,8 @@ images = mnistDir + "/train-images-idx3-ubyte"
 labels = mnistDir + "/train-labels-idx1-ubyte"
 
 # Test
-images = mnistDir + "/t10k-images-idx3-ubyte"
-labels = mnistDir + "/t10k-labels-idx1-ubyte"
+# images = mnistDir + "/t10k-images-idx3-ubyte"
+# labels = mnistDir + "/t10k-labels-idx1-ubyte"
 
 # Where to store the trained hyper-parameters and the time/accuracy results
 paramDir = "./parameters"
