aa7756cac2fbcb01121c6221c7c4ea8a087e3565
alessio <alessio.carpegna@polito.it>
2023-01-11 16:17:30 +0100

    Working training evaluation.

--- Python/Custom/FullPrecision/Evaluation/trainingEvaluation.py
+++ Python/Custom/FullPrecision/Evaluation/trainingEvaluation.py
@@ -2,6 +2,9 @@ import sys
 import scipy.sparse as sp
 import matplotlib.pyplot as plt
 
+# Initialize the evaluation parameters
+from runParameters import *
+
 development = "../"
 
 if development not in sys.path:
@@ -12,14 +15,11 @@ from createNetwork import createNetwork
 from trainFunctions import run
 
 
-# Initialize the evaluation parameters
-from evaluationParameters import *
 
 
 # Create the network
-network = createNetwork(networkList, None, None, mode, excDictList, 
-			inhDictList, scaleFactors, exc2inhWeights,
-			inh2excWeights)
+network = createNetwork(networkList, None, None, mode, 
+			excDictList, scaleFactors, inh2excWeights)
 
 
 # Create a spare matrix of random spikes
@@ -27,6 +27,7 @@ spikesTrains = sp.random(N_sim, networkList[0], density = density)
 spikesTrains = spikesTrains.A.astype(bool)
 
 
-counter = run(network, networkList, spikesTrains, dt_tauDict, stdpDict, mode)
+counter = run(network, networkList, spikesTrains, dt_tauDict, stdpDict, mode,
+		constSums)
 
 print("Counter: ", counter)
