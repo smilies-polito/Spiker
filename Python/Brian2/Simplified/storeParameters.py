9842f81d830e35dc7fff0ea860cb7543f7a3fb0e
alessio <alessio.carpegna@polito.it>
2023-01-11 11:08:36 +0100

    Check comments store parameters.

--- Python/Brian2/Simplified/storeParameters.py
+++ Python/Brian2/Simplified/storeParameters.py
@@ -6,7 +6,7 @@ from utils import seconds2hhmmss
 def storeParameters(network, networkList, assignments, weightFilename,
 			thetaFilename, assignmentsFile):
 
-	'''
+	"""
 	Store the parameters of the network inside NumPy files.
 
 	INPUT:
@@ -31,7 +31,7 @@ def storeParameters(network, networkList, assignments, weightFilename,
 		6) assignmentsFile: string. Complete name of the file in which
 		the assignments of the output layer will be stored.
 	
-	'''
+	"""
 
 	for layer in range(1, len(networkList)):
 
@@ -53,7 +53,7 @@ def storeParameters(network, networkList, assignments, weightFilename,
 
 def storeArray(filename, numpyArray):
 
-	'''
+	"""
 	Store a NumPy array into a NumPy file.
 
 	INPUT:
@@ -62,9 +62,9 @@ def storeArray(filename, numpyArray):
 		stored. The standard requires ".npy" as file extension.
 
 		2) numpyArray: NumPy array that will be stored into the file.
-	'''
+	"""
 
-	with open(filename, 'wb') as fp:
+	with open(filename, "wb") as fp:
 		np.save(fp, numpyArray)
 
 
@@ -73,7 +73,7 @@ def storeArray(filename, numpyArray):
 
 def storePerformace(startTimeTraining, accuracies, performanceFile):
 
-	'''
+	"""
 	Store the performance of the network into a text file.
 
 	INPUT:
@@ -87,7 +87,7 @@ def storePerformace(startTimeTraining, accuracies, performanceFile):
 		3) performanceFile: string. Complete name of the file in which
 		the performance of the network will be stored.
 
-	'''
+	"""
 	
 	# Format the string with the total elapsed time
 	timeString = "Total training time : " + \
@@ -97,7 +97,7 @@ def storePerformace(startTimeTraining, accuracies, performanceFile):
 	accuracyString = "Accuracy evolution:\n" + "\n".join(accuracies)
 
 	# Write the strings into the file
-	with open(performanceFile, 'w') as fp:
+	with open(performanceFile, "w") as fp:
 		fp.write(timeString)
 		fp.write("\n\n")
 		fp.write(accuracyString)
