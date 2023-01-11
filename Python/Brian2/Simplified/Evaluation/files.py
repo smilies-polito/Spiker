01d32f3a4bc1fdb1f8048f41025d8d2fc1265bfe
alessio <alessio.carpegna@polito.it>
2023-01-11 11:24:44 +0100

    Working test functions.

--- Python/Brian2/Simplified/Evaluation/files.py
+++ Python/Brian2/Simplified/Evaluation/files.py
@@ -1,12 +1,22 @@
-# Complete path of the MNIST files
-trainImages = "../../../..//brian2/mnist/train-images-idx3-ubyte"
-trainLabels = "../../../../brian2/mnist/train-labels-idx1-ubyte"
-testImages = "../../../../brian2/mnist/t10k-images-idx3-ubyte"
-testLabels = "../../../../brian2/mnist/t10k-labels-idx1-ubyte"
+developmentDir = "../"
+
+mnistDir = "../../../../MNIST"
+
+# Training
+trainImages = mnistDir + "/train-images-idx3-ubyte"
+trainLabels = mnistDir + "/train-labels-idx1-ubyte"
+
+# Test
+testImages = mnistDir + "/t10k-images-idx3-ubyte"
+testLabels = mnistDir + "/t10k-labels-idx1-ubyte"
+
+
+# Directory containing equations and their parameters
+equationsDir = "../Equations"
 
 
 # Directory in which parameters and performance of the network are stored
-paramDir = "../parameters"
+paramDir = "./Parameters"
 
 
 # Name of the parameters files
@@ -18,4 +28,3 @@ assignmentsFile = paramDir + "/assignments.npy"
 # Name of the performance files
 trainPerformanceFile = paramDir + "/performance.txt"
 testPerformanceFile = paramDir + "/testPerformance.txt"
-
--- Python/Brian2/Simplified/Evaluation/test.py
+++ Python/Brian2/Simplified/Evaluation/test.py
@@ -2,16 +2,19 @@ import numpy as np
 import brian2 as b2
 import timeit
 import subprocess
+from files import *
 
 import sys
 
-development = "../"
-
-if development not in sys.path:
-	sys.path.insert(1,development)
-
+if mnistDir not in sys.path:
+	sys.path.append(mnistDir)
 
 from mnist import loadDataset
+
+if developmentDir not in sys.path:
+	sys.path.insert(1,developmentDir)
+
+
 from createNetwork import createNetwork
 from runFunctions import singleImageRun
 from storeParameters import storeArray
@@ -22,10 +25,12 @@ from poisson import imgToSpikeTrain
 
 networkList = [784, 400]
 
-storeArray("../parameters/assignments.npy", -1*np.ones(networkList[-1]))
-storeArray("../parameters/weights1.npy", b2.random(networkList[1]*networkList[0])
+
+createDir(paramDir)
+storeArray(assignmentsFile, -1*np.ones(networkList[-1]))
+storeArray(weightFilename + "1.npy", b2.random(networkList[1]*networkList[0])
 + 0.01)
-storeArray("../parameters/theta1.npy", -1*np.ones(networkList[-1]))
+storeArray(thetaFilename + "1.npy", -1*np.ones(networkList[-1]))
 
 from runParameters import *
 
