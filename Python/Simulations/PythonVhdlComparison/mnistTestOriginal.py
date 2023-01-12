048b0b075311bfcfd55eda41e34a85a13213b9e0
alessio <alessio.carpegna@polito.it>
2023-01-12 09:32:44 +0100

    Create directory dedicated to python simulations.

---
+++ Python/Simulations/PythonVhdlComparison/mnistTestOriginal.py
@@ -0,0 +1,75 @@
+import timeit
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+np.set_printoptions(threshold=np.inf)
+
+from mnist import loadDataset
+from createNetwork import createNetwork
+from testFunctionsOriginal import singleImageTest
+from storeParameters import *
+from utils import checkParallelism
+
+
+
+
+# Initialize the training parameters
+from files import *
+from runParameters import *
+
+
+
+# Load the MNIST dataset
+imgArray , labelsArray = loadDataset(testImages, testLabels)
+
+
+# Create the network data structure
+network = createNetwork(networkList, weightFilename, thresholdFilename, mode, 
+			excDictList, scaleFactors, inh2excWeights,
+			fixed_point_decimals, trainPrecision, rng)
+
+checkParallelism(network["exc2exc1"]["weights"], weights_parallelism)
+
+currentIndex = 0
+numberOfCycles = imgArray.shape[0]
+
+
+# Measure the test starting time
+startTimeTraining = timeit.default_timer()
+
+
+while currentIndex < numberOfCycles:
+
+	# Complete test cycle over a single image
+	inputIntensity, currentIndex, accuracies, spikesMonitor, \
+		membraneMonitor = \
+		singleImageTest(
+			trainDuration,
+			restTime,
+			dt,
+			imgArray[currentIndex],
+			network,
+			networkList,
+			dt_tauDict,
+			countThreshold,
+			inputIntensity,
+			currentIndex,
+			spikesEvolution,
+			updateInterval,
+			printInterval,
+			startTimeTraining,
+			accuracies,
+			labelsArray,
+			assignments,
+			startInputIntensity,
+			mode,
+			constSums,
+			rng,
+			exp_shift,
+			neuron_parallelism,
+			inputFilename
+		)
+
+
+# Store the performance of the network into a text file
+storePerformace(startTimeTraining, accuracies, testPerformanceFile)
---
+++ Python/Simulations/PythonVhdlComparison/parallelismCheck.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+parallelismFile="parallelisms.py"
+tmpFile="tmp.py"
+mnistTest="mnistTestOriginal.py"
+
+for i in {10..32}
+do
+	# Substitute the parallelism value and store in temporary file
+	sed s/"neuron_parallelism.*"/"neuron_parallelism	= $i"/\
+	$parallelismFile > $tmpFile
+
+	# Remove previous file
+	rm $parallelismFile
+
+	# Rename the temporary file
+	mv $tmpFile $parallelismFile
+
+	echo -e "Parallelism: $i\n"
+	python $mnistTest
+	echo -e "\n"
+done
