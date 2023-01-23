ddb747208afa34fcdaf1f0bcc4896eec5e8ef72e
alessio <alessio.carpegna@polito.it>
2023-01-23 12:25:22 +0100

    Update paths in debug compile.

--- Vhdl/Hierarchical/Sim/VivadoScripts/debug_compile.sh
+++ Vhdl/Hierarchical/Sim/VivadoScripts/debug_compile.sh
@@ -1,55 +1,55 @@
 #!/usr/bin/bash
 
-VHD_DIR="../../vhd/vhd"
-DEBUG_DIR="../../vhd/debug"
-TB_DIR="../../tb/debug/layer"
+VHD_DIR="../../Vhd/Vhd"
+DEBUG_DIR="../../Vhd/Debug"
+TB_DIR="../../Tb/Debug/Layer"
 
 TOP_ENTITY="debug_layer_tb"
 
 # Neuron
-xvhdl $VHD_DIR"/neuron/add_sub.vhd"
-xvhdl $VHD_DIR"/neuron/cmp_gt.vhd"
-xvhdl $VHD_DIR"/neuron/mux2to1_signed.vhd"
-xvhdl $VHD_DIR"/neuron/mux4to1_signed.vhd"
-xvhdl $VHD_DIR"/neuron/neuron_cu.vhd"
-xvhdl $VHD_DIR"/neuron/neuron_datapath.vhd"
-xvhdl $VHD_DIR"/neuron/neuron.vhd"
-xvhdl $DEBUG_DIR"/neuron/debug_neuron_datapath.vhd"
-xvhdl $DEBUG_DIR"/neuron/debug_neuron.vhd"
-xvhdl $VHD_DIR"/neuron/reg_signed_sync_rst.vhd"
-xvhdl $VHD_DIR"/neuron/shifter.vhd"
+xvhdl $VHD_DIR"/Neuron/add_sub.vhd"
+xvhdl $VHD_DIR"/Neuron/cmp_gt.vhd"
+xvhdl $VHD_DIR"/Neuron/mux2to1_signed.vhd"
+xvhdl $VHD_DIR"/Neuron/mux4to1_signed.vhd"
+xvhdl $VHD_DIR"/Neuron/neuron_cu.vhd"
+xvhdl $VHD_DIR"/Neuron/neuron_datapath.vhd"
+xvhdl $VHD_DIR"/Neuron/neuron.vhd"
+xvhdl $DEBUG_DIR"/Neuron/debug_neuron_datapath.vhd"
+xvhdl $DEBUG_DIR"/Neuron/debug_neuron.vhd"
+xvhdl $VHD_DIR"/Neuron/reg_signed_sync_rst.vhd"
+xvhdl $VHD_DIR"/Neuron/shifter.vhd"
 
 # Bare neurons
-xvhdl $DEBUG_DIR"/bare_neurons/debug_bare_neurons.vhd"
-xvhdl $VHD_DIR"/bare_neurons/generic_and.vhd"
+xvhdl $DEBUG_DIR"/BareNeurons/debug_bare_neurons.vhd"
+xvhdl $VHD_DIR"/BareNeurons/generic_and.vhd"
 
 # Complete neurons
-xvhdl $DEBUG_DIR"/complete_neurons/debug_complete_neurons.vhd"
-xvhdl $VHD_DIR"/complete_neurons/double_and_mask_n.vhd"
-xvhdl $VHD_DIR"/complete_neurons/simple_and_mask.vhd"
+xvhdl $DEBUG_DIR"/CompleteNeurons/debug_complete_neurons.vhd"
+xvhdl $VHD_DIR"/CompleteNeurons/double_and_mask_n.vhd"
+xvhdl $VHD_DIR"/CompleteNeurons/simple_and_mask.vhd"
 
 # Layer
-xvhdl $VHD_DIR"/layer/anticipate_bits.vhd"
-xvhdl $VHD_DIR"/layer/bit_selection.vhd"
-xvhdl $VHD_DIR"/layer/cmp_eq.vhd"
-xvhdl $VHD_DIR"/layer/cnt.vhd"
-xvhdl $VHD_DIR"/layer/generic_mux_1bit.vhd"
-xvhdl $VHD_DIR"/layer/generic_or.vhd"
-xvhdl $DEBUG_DIR"/layer/debug_layer.vhd"
-xvhdl $VHD_DIR"/layer/layer_cu.vhd"
-xvhdl $DEBUG_DIR"/layer/debug_layer_datapath.vhd"
-xvhdl $VHD_DIR"/layer/mux2to1.vhd"
-xvhdl $VHD_DIR"/layer/reg.vhd"
+xvhdl $VHD_DIR"/Layer/anticipate_bits.vhd"
+xvhdl $VHD_DIR"/Layer/bit_selection.vhd"
+xvhdl $VHD_DIR"/Layer/cmp_eq.vhd"
+xvhdl $VHD_DIR"/Layer/generic_mux_1bit.vhd"
+xvhdl $VHD_DIR"/Layer/generic_or.vhd"
+xvhdl $DEBUG_DIR"/Layer/debug_layer.vhd"
+xvhdl $VHD_DIR"/Layer/layer_cu.vhd"
+xvhdl $DEBUG_DIR"/Layer/debug_layer_datapath.vhd"
+xvhdl $VHD_DIR"/Layer/mux2to1.vhd"
+xvhdl $VHD_DIR"/Layer/reg.vhd"
 
 # Synapses
-xvhdl $VHD_DIR"/synapse/weights_bram.vhd"
+xvhdl $VHD_DIR"/Synapse/weights_bram.vhd"
 
 # Shared components
-xvhdl $VHD_DIR"/shared/decoder.vhd"
-xvhdl $VHD_DIR"/shared/mux2to1_std_logic.vhd"
-xvhdl $VHD_DIR"/shared/reg_signed.vhd"
+xvhdl $VHD_DIR"/Shared/decoder.vhd"
+xvhdl $VHD_DIR"/Shared/mux2to1_std_logic.vhd"
+xvhdl $VHD_DIR"/Shared/reg_signed.vhd"
+xvhdl $VHD_DIR"/Shared/cnt.vhd"
 
 # Testbench
-xvhdl $VHD_DIR"/io/load_file.vhd"
+xvhdl $VHD_DIR"/IO/load_file.vhd"
 
 xvhdl $TB_DIR"/debug_layer_tb.vhd"
