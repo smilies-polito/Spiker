c3154899884a1a4d6797df061d41b5103a66d459
alessio <alessio.carpegna@polito.it>
2023-01-24 15:56:57 +0100

    Fix equation paths in Brian 2.

--- Python/Models/Brian2/PeterDiehl/files.py
+++ Python/Models/Brian2/PeterDiehl/files.py
@@ -8,6 +8,10 @@ labels = mnistDir + "/train-labels-idx1-ubyte"
 # images = mnistDir + "/t10k-images-idx3-ubyte"
 # labels = mnistDir + "/t10k-labels-idx1-ubyte"
 
+
+equationsDir = "./Equations"
+
+
 # Where to store the trained hyper-parameters and the time/accuracy results
 paramDir = "./Parameters"
 
