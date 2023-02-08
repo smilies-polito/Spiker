dd7bf9209211ccbae3c53d0c9bf40fd1e5599ab2
alessio <alessio.carpegna@polito.it>
2023-02-08 16:46:02 +0100

    Try different model parameters.
    Network architecture: 	= 784 - 200 -100 - 10
    Batch size 		= 256
    Epochs			= 100

--- Python/Models/SnnTorch/runParameters.py
+++ Python/Models/SnnTorch/runParameters.py
@@ -1,13 +1,13 @@
 import torch
 
 # dataloader arguments
-batch_size = 128
+batch_size = 256
 
 dtype = torch.float
 
 # Network Architecture
 num_inputs = 28*28
-num_hidden1 = 400
+num_hidden1 = 200
 num_hidden2 = 100
 num_outputs = 10
 
@@ -15,7 +15,7 @@ num_outputs = 10
 num_steps = 25
 beta = 0.95
 
-num_epochs = 10
+num_epochs = 100
 loss_hist = []
 test_loss_hist = []
 counter = 0
