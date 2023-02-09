942113fa18b64c55fe57960fbd8b11b71337eb4b
alessio <alessio.carpegna@polito.it>
2023-02-09 16:01:59 +0100

    Supervised spiker working.
    	1. Architecture	= 784 - 400 - 10
    	2. Beta 	= 1 - 2^(-4) = 0.9375  --> Delta_t / tau = >>4
    
    I start from here. Now I want to understand the internals of snntorch, to
    translate them into my custom model and I need to add softmax to the output.

--- Python/Models/SnnTorch/runParameters.py
+++ Python/Models/SnnTorch/runParameters.py
@@ -7,15 +7,14 @@ dtype = torch.float
 
 # Network Architecture
 num_inputs = 28*28
-num_hidden1 = 200
-num_hidden2 = 100
+num_hidden = 400
 num_outputs = 10
 
 # Temporal Dynamics
 num_steps = 25
-beta = 0.95
+beta = 0.9375
 
-num_epochs = 100
+num_epochs = 1000
 loss_hist = []
 test_loss_hist = []
 counter = 0
