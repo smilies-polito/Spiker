d85dcff97ab5f649edcfb7feb47fe888dfa3d222
alessio <alessio.carpegna@polito.it>
2023-02-10 15:36:25 +0100

    Add parameters require from custom scripts.

--- Python/Models/Custom/FullPrecisionSupervised/runParameters.py
+++ Python/Models/Custom/FullPrecisionSupervised/runParameters.py
@@ -1,4 +1,8 @@
 import torch
+import numpy as np
+
+from parameters import *
+from files import *
 
 # dataloader arguments
 batch_size = 256
@@ -18,3 +22,53 @@ num_epochs = 100
 loss_hist = []
 test_loss_hist = []
 counter = 0
+
+# List of layer sizes
+networkList = [784, 400, 10]
+
+# List of dictionaries of parameters for the layers
+excDictList = [excDict.copy()] * (len(networkList) - 1)
+
+# Arrays of weights for the inter layer connections
+inh2excWeights = np.ones(len(networkList) - 1)
+
+# Training and resting periods in milliseconds
+trainDuration = 350	# ms
+
+# Time step duration in milliseconds
+dt = 0.1		# ms
+
+# Exponential time constants
+dt_tauDict = {
+	"exc" 		: dt/tauExc,
+}
+
+# Array of scale factors for the random generation of the weights
+scaleFactors = np.ones(len(networkList) - 1)
+
+
+# Update and print intervals expressed in number of images
+printInterval = 10
+updateInterval = 250
+
+# Initialize history of accuracies
+accuracies = []
+
+# Initialize history of spikes
+spikesEvolution = np.zeros((updateInterval, networkList[-1]))
+
+# Minimum acceptable number of output spikes generated during the training.
+countThreshold = 3
+
+# NumPy default random generator.
+rng = np.random.default_rng()
+
+
+for i in range(1, len(networkList)):
+	thresholds = np.ones((1, networkList[i]))*excDict["vThresh0"]
+	np.save(thresholdsFilename + str(i) + ".npy", thresholds)
+
+
+for i in range(1, len(networkList)):
+	weights = np.ones((networkList[i-1], networkList[i]))
+	np.save(weightsFilename + str(i) + ".npy", thresholds)
