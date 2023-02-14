6b056eabb6638d0be4607ff07e48ef79573db0dd
alessio <alessio.carpegna@polito.it>
2023-02-14 14:34:57 +0100

    Input/output files.
    Import spikes from file. Store counters and label on file.

--- Python/Models/Custom/FullPrecisionSupervised/files.py
+++ Python/Models/Custom/FullPrecisionSupervised/files.py
@@ -3,3 +3,8 @@ data_path='./data/mnist'
 paramDir = "./Parameters"
 weightsFilename = paramDir + "/weights"
 thresholdsFilename = paramDir + "/thresholds"
+
+spikesDir = "../../../Simulations/PythonSim/SnnTorchVScustom/Spikes"
+inputSpikes = spikesDir + "/spikes.pt"
+
+logFile = "../../../Simulations/PythonSim/SnnTorchVScustom/Output/customLog.txt"
