653618971efc619d9b767940f13e7961fdb06b5e
alessio <alessio.carpegna@polito.it>
2023-02-14 10:44:38 +0100

    Rename simulation scripts.

--- Python/Simulations/PythonSim/SnnTorchVScustom/files.py
+++ Python/Simulations/PythonSim/SnnTorchVScustom/files.py
@@ -3,5 +3,5 @@ data_path='./data/mnist'
 spikesDir = "./Spikes"
 spikesFilename = spikesDir + "/spikes.pt"
 
-custom = "./runCustom.sh"
-snntorch_model = "./runSnnTorchModel.sh"
+custom = "./custom.sh"
+snntorch_model = "./snnTorchModel.sh"
