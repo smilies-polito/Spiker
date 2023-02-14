506d4e1d10056b50cd364df20b53714c25896b52
alessio <alessio.carpegna@polito.it>
2023-02-14 10:32:10 +0100

    Store spikes on file.

--- Python/Simulations/PythonSim/SnnTorchVScustom/spikesGen.py
+++ Python/Simulations/PythonSim/SnnTorchVScustom/spikesGen.py
@@ -5,9 +5,12 @@ import torch
 import torch.nn as nn
 
 from mnist import loadDataset
+from utils import createDir
 
 from runParameters import *
 from files import *
+
+createDir(spikesDir)
 
 train_loader, test_loader = loadDataset(data_path, batch_size)
 
@@ -16,3 +19,6 @@ test_batch = iter(test_loader)
 for test_data, test_targets in test_batch:
 
 	input_spikes = spikegen.rate(test_data, num_steps = num_steps, gain = 1)
+
+	with open(spikesFilename, "wb") as fp:
+		torch.save(input_spikes, fp)
