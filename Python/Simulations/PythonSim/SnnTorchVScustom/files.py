586cc571ef5fb9e34976014d8d6b90e02b06c4ab
alessio <alessio.carpegna@polito.it>
2023-02-14 10:36:24 +0100

    Run bash simulation scripts.

--- Python/Simulations/PythonSim/SnnTorchVScustom/files.py
+++ Python/Simulations/PythonSim/SnnTorchVScustom/files.py
@@ -2,3 +2,6 @@ data_path='./data/mnist'
 
 spikesDir = "./Spikes"
 spikesFilename = spikesDir + "/spikes.pt"
+
+custom = "./runCustom.sh"
+snntorch_model = "./runSnnTorchModel.sh"
--- Python/Simulations/PythonSim/SnnTorchVScustom/spikesGen.py
+++ Python/Simulations/PythonSim/SnnTorchVScustom/spikesGen.py
@@ -12,13 +12,19 @@ from files import *
 
 createDir(spikesDir)
 
+# Import dataset
 train_loader, test_loader = loadDataset(data_path, batch_size)
-
 test_batch = iter(test_loader)
 
+# Loop over the batches
 for test_data, test_targets in test_batch:
 
+	# Generate spikes
 	input_spikes = spikegen.rate(test_data, num_steps = num_steps, gain = 1)
 
+	# Store spikes on file
 	with open(spikesFilename, "wb") as fp:
 		torch.save(input_spikes, fp)
+
+	sp.run(custom, shell = True, executable = "/bin/bash")
+	sp.run(snntorch_model, shell = True, executable = "/bin/bash")
