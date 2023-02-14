c3f89b6419652edb087a888977ddb4c926437bb0
alessio <alessio.carpegna@polito.it>
2023-02-14 14:33:18 +0100

    Prompt user during the simulation process.
    Ask if a he wants to generate a new set of spikes. Ask if he wants to continue
    the simulation.

--- Python/Simulations/PythonSim/SnnTorchVScustom/spikesGen.py
+++ Python/Simulations/PythonSim/SnnTorchVScustom/spikesGen.py
@@ -12,7 +12,7 @@ from utils import createDir
 from runParameters import *
 from files import *
 
-createDir(spikesDir)
+createDir(logDir)
 
 # Import dataset
 train_loader, test_loader = loadDataset(data_path, batch_size)
@@ -21,16 +21,23 @@ test_batch = iter(test_loader)
 # Loop over the batches
 for test_data, test_targets in test_batch:
 
-	# Generate spikes
-	input_spikes = spikegen.rate(test_data, num_steps = num_steps, gain = 1)
+	answer = input("Generate new set of spikes? (y/[n])")
 
-	# Store spikes on file
-	with open(spikesFilename, "wb") as fp:
-		torch.save(input_spikes, fp)
+	if answer == "y":
+
+		createDir(spikesDir)
+
+		# Generate spikes
+		input_spikes = spikegen.rate(test_data.view(batch_size, -1),
+				num_steps = num_steps, gain = 1)
+
+		# Store spikes on file
+		with open(spikesFilename, "wb") as fp:
+			torch.save(input_spikes, fp)
 
 	sp.run(custom, shell = True, executable = "/bin/bash")
 	sp.run(snntorch_model, shell = True, executable = "/bin/bash")
 
-	answer = input("\nContinue([y]/n)? ")
+	answer = input("\nContinue? ([y]/n) ")
 	if answer == "n":
 		break
