e8dff4da68a007ef93847889fe77ec28ced62ea1
alessio <alessio.carpegna@polito.it>
2023-02-14 10:04:48 +0100

    Import the dataset and loop over batches.
    Convert inputs in spikes in batches.

--- Python/Simulations/PythonSim/SnnTorchVScustom/spikesGen.py
+++ Python/Simulations/PythonSim/SnnTorchVScustom/spikesGen.py
@@ -4,8 +4,15 @@ from snntorch import spikegen
 import torch
 import torch.nn as nn
 
+from mnist import loadDataset
+
 from runParameters import *
 from files import *
 
+train_loader, test_loader = loadDataset(data_path, batch_size)
 
-input_spikes = spikegen.rate(data_it, num_steps = num_steps, gain = 1)
+test_batch = iter(test_loader)
+
+for test_data, test_targets in test_batch:
+
+	input_spikes = spikegen.rate(test_data, num_steps = num_steps, gain = 1)
