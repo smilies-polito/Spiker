9114e08a931459fa790687dfd9f0c8bf1988cf04
alessio <alessio.carpegna@polito.it>
2023-02-15 11:20:42 +0100

    Remove BIAS from linear layer.
    I was wondering why running multiple experiments caused different temporal
    evolutions in the membrane potential. The linear layer initializes a random bias
    when created.
    
    Add a print of the output of the linear layer: there are still some differences
    with respect to the custom model.

--- Python/Models/SnnTorch/network.py
+++ Python/Models/SnnTorch/network.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import snntorch as snn
 from snntorch import spikegen
 
@@ -12,9 +14,9 @@ class Net(nn.Module):
 		super().__init__()
 
 		# Initialize layers
-		self.fc1 = nn.Linear(num_inputs, num_hidden)
+		self.fc1 = nn.Linear(num_inputs, num_hidden, bias = False)
 		self.lif1 = snn.Leaky(beta=beta)
-		self.fc2 = nn.Linear(num_hidden, num_outputs)
+		self.fc2 = nn.Linear(num_hidden, num_outputs, bias = False)
 		self.lif2 = snn.Leaky(beta=beta)
 
 	def forward(self, data_it, num_steps):
@@ -32,6 +34,7 @@ class Net(nn.Module):
 
 		for step in range(num_steps):
 			cur1 = self.fc1(input_spikes[step])
+			print(float(cur1[step, 0]))
 			spk1, mem1 = self.lif1(cur1, mem1)
 			cur2 = self.fc2(spk1)
 			spk2, mem2 = self.lif2(cur2, mem2)
