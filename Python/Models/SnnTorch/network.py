e6cec943c4d2a6af43d51490b153cf3bed38faf9
alessio <alessio.carpegna@polito.it>
2023-02-16 14:28:16 +0100

    Try a shallower but smaller architecture.
    It seems that deeper architectures are quite slow to learn. Let's try with a
    smaller architecture:
    
    	784 - 128 - 10

--- Python/Models/SnnTorch/network.py
+++ Python/Models/SnnTorch/network.py
@@ -6,27 +6,24 @@ import torch.nn as nn
 
 # Define Network
 class Net(nn.Module):
-	def __init__(self, num_inputs, num_hidden1, num_hidden2, num_outputs, beta):
+	def __init__(self, num_inputs, num_hidden, num_outputs, beta):
 		super().__init__()
 
 		# Initialize layers
-		self.fc1 = nn.Linear(num_inputs, num_hidden1)
+		self.fc1 = nn.Linear(num_inputs, num_hidden)
 		self.lif1 = snn.Leaky(beta=beta)
-		self.fc2 = nn.Linear(num_hidden1, num_hidden2)
+		self.fc2 = nn.Linear(num_hidden, num_outputs)
 		self.lif2 = snn.Leaky(beta=beta)
-		self.fc3 = nn.Linear(num_hidden2, num_outputs)
-		self.lif3 = snn.Leaky(beta=beta)
 
 	def forward(self, data_it, num_steps):
 
 		# Initialize hidden states at t=0
 		mem1 = self.lif1.init_leaky()
 		mem2 = self.lif2.init_leaky()
-		mem3 = self.lif2.init_leaky()
 
 		# Record the final layer
-		spk3_rec = []
-		mem3_rec = []
+		spk2_rec = []
+		mem2_rec = []
 
 		input_spikes = spikegen.rate(data_it, num_steps = num_steps,
 				gain = 1)
@@ -36,10 +33,8 @@ class Net(nn.Module):
 			spk1, mem1 = self.lif1(cur1, mem1)
 			cur2 = self.fc2(spk1)
 			spk2, mem2 = self.lif2(cur2, mem2)
-			cur3 = self.fc3(spk2)
-			spk3, mem3 = self.lif3(cur3, mem3)
-			spk3_rec.append(spk3)
-			mem3_rec.append(mem3)
+			spk2_rec.append(spk2)
+			mem2_rec.append(mem2)
 
-		return torch.stack(spk3_rec, dim=0), torch.stack(mem3_rec, dim=0)
+		return torch.stack(spk2_rec, dim=0), torch.stack(mem2_rec, dim=0)
 
