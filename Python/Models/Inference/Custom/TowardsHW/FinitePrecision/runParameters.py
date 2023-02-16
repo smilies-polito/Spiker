495d314fbf161cc66e7a49df99f8b92d4768ed30
alessio <alessio.carpegna@polito.it>
2023-02-16 15:45:49 +0100

    Same parameters used for training.

--- Python/Models/Inference/Custom/TowardsHW/FinitePrecision/runParameters.py
+++ Python/Models/Inference/Custom/TowardsHW/FinitePrecision/runParameters.py
@@ -1,12 +1,32 @@
 import numpy as np
+import torch
 from parameters import *
 from utils import initAssignments
 
 from files import *
 
 
+# dataloader arguments
+batch_size = 256
+
+dtype = torch.float
+
+# Network Architecture
+num_inputs = 28*28
+num_hidden = 400
+num_outputs = 10
+
+# Temporal Dynamics
+num_steps = 25
+beta = 0.9375
+
+num_epochs = 100
+loss_hist = []
+test_loss_hist = []
+counter = 0
+
 # List of layer sizes
-networkList = [784, 400]
+networkList = [784, 400, 10]
 
 mode = "test"
 trainPrecision = "float"
@@ -14,33 +34,22 @@ trainPrecision = "float"
 # List of dictionaries of parameters for the layers
 excDictList = [excDict] * (len(networkList) - 1)
 
-
-# Arrays of weights for the inter layer connections
-inh2excWeights = inh2excWeight * np.ones(len(networkList) - 1)
-
-
 # Training and resting periods in milliseconds
 trainDuration = 350	# ms
-restTime = 150		# ms
+
+# Array of scale factors for the random generation of the weights
+scaleFactors = np.ones(len(networkList) - 1)
+
 
 
 # Time step duration in milliseconds
-dt = 0.1		# ms
+dt = 2**-4 * tauExc		# ms
 
 
 # Exponential time constants
 dt_tauDict = {
-	"exc" 		: 2**(-10)#dt/tauExc,
+	"exc" 		: dt/tauExc,
 }
-
-
-stdpDict["ltp_dt_tau"] = dt/stdpDict["ltp_tau"]
-stdpDict["ltd_dt_tau"] = dt/stdpDict["ltd_tau"]
-
-
-# Array of scale factors for the random generation of the weights
-scaleFactors = scaleFactor * np.ones(len(networkList) - 1)
-
 
 # Update and print intervals expressed in number of images
 updateInterval = 10
@@ -52,19 +61,11 @@ startInputIntensity = 2.
 inputIntensity = startInputIntensity
 
 
-# Array of normalization factors for the weights of the various layers 
-constSums = constSum * np.ones(len(networkList) - 1)
-
-
 # Initialize history of accuracies
 accuracies = []
 
 # Initialize history of spikes
 spikesEvolution = np.zeros((updateInterval, networkList[-1]))
-
-
-# Initialize the output classification
-assignments = initAssignments(mode, networkList, assignmentsFile)
 
 
 # Minimum acceptable number of output spikes generated during the training.
