1452cb280aa32a1221b376ec9f37a433160f00c2
alessio <alessio.carpegna@polito.it>
2023-06-13 17:20:08 +0200

    Complete neuron modified.
    1) Connect datapath and control unit
    
    2) Mask weights with the input spikes. Add the weights only when there is an
    active spike.
    
    3) Mask the threshold upload: allow it only in the load state, when load_ready
    is high.

--- Vhdl/Hierarchical/Vhd/Vhd/Neuron/neuron.vhd
+++ Vhdl/Hierarchical/Vhd/Vhd/Neuron/neuron.vhd
@@ -15,18 +15,20 @@ entity neuron is
 	);
 
 	port(
+
+		-- input spike
+		exc_spike		: in std_logic;
+		inh_spike		: in std_logic;
+
 		-- input controls
 		clk			: in std_logic;
 		rst_n			: in std_logic;
-		start			: in std_logic;
-		stop			: in std_logic;
-		exc_or			: in std_logic;
-		exc_stop		: in std_logic;
-		inh_or			: in std_logic;
-		inh_stop		: in std_logic;
-		input_spike		: in std_logic;
+		restart			: in std_logic;
+		load_end		: in std_logic;
+		exc			: in std_logic;
+		inh			: in std_logic;
 
-		-- to load the threshold
+		-- direct control on the datapath
 		v_th_en			: in std_logic;
 
 		-- input parameters
@@ -34,14 +36,15 @@ entity neuron is
 						downto 0);
 		v_reset			: in signed(neuron_bit_width-1
 						downto 0);
-		inh_weight		: in signed(neuron_bit_width-1
+		inh_weight		: in signed(weights_bit_width-1
 						downto 0);
 		exc_weight		: in signed(weights_bit_width-1
 						downto 0);
 
 		-- output
-		out_spike		: out std_logic;
-		neuron_ready		: out std_logic
+		load_ready		: out std_logic;
+		neuron_ready		: out std_logic;
+		out_spike		: out std_logic
 	);
 
 end entity neuron;
@@ -51,20 +54,55 @@ architecture behaviour of neuron is
 
 
 	-- from control unit towards datapath
-	signal update_sel	: std_logic_vector(1 downto 0);
-	signal add_or_sub	: std_logic;
-	signal v_update		: std_logic;
-	signal v_en		: std_logic;
-	signal v_rst_n		: std_logic;
+	signal update_sel		: std_logic_vector(1 downto 0);
+	signal add_or_sub		: std_logic;
+	signal v_update			: std_logic;
+	signal v_en			: std_logic;
+	signal v_rst_n			: std_logic;
 
 	-- from datapath towards control unit
-	signal exceed_v_th	: std_logic;
+	signal exceed_v_th		: std_logic;
 
+	-- masking signals
+	signal load_en			: std_logic;
+	signal masked_v_th_en		: std_logic;
+	signal masked_exc_weight	: signed(weights_bit_width-1 downto 0);
+	signal masked_inh_weight	: signed(weights_bit_width-1 downto 0);
+
+
+	component neuron_cu is
+
+		port(
+			-- input
+			clk		: in std_logic;
+			rst_n		: in std_logic;
+			restart		: in std_logic;
+			load_end	: in std_logic;
+			exc		: in std_logic;
+			inh		: in std_logic;
+
+			-- input from datapath
+			exceed_v_th	: in std_logic;
+
+			-- control output
+			update_sel	: out std_logic_vector(1 downto 0);
+			add_or_sub	: out std_logic;
+			v_update	: out std_logic;
+			v_en		: out std_logic;
+			v_rst_n		: out std_logic;
+
+			-- output
+			load_ready	: out std_logic;
+			neuron_ready	: out std_logic;
+			out_spike	: out std_logic
+		);
+
+	end component neuron_cu;
 
 	component neuron_datapath is
 
 		generic(
-			-- parallelism
+			-- bit-width
 			neuron_bit_width		: integer := 16;
 			weights_bit_width		: integer := 5;
 
@@ -78,15 +116,14 @@ architecture behaviour of neuron is
 							downto 0);
 			v_reset			: in signed(neuron_bit_width-1 
 							downto 0);
-			inh_weight		: in signed(neuron_bit_width-1
+			inh_weight		: in signed(weights_bit_width-1
 							downto 0);
 			exc_weight		: in signed(weights_bit_width-1
 							downto 0);
 
 			-- input controls
 			clk			: in std_logic;
-			update_sel		: in std_logic_vector(1 
-							downto 0);
+			update_sel		: in std_logic_vector(1 downto 0);
 			add_or_sub		: in std_logic;
 			v_update		: in std_logic;
 			v_th_en			: in std_logic;
@@ -100,100 +137,84 @@ architecture behaviour of neuron is
 	end component neuron_datapath;
 
 
-	component neuron_cu is
-
-		port(
-			-- input
-			clk		: in std_logic;
-			rst_n		: in std_logic;
-			start		: in std_logic;
-			stop		: in std_logic;
-			exc_or		: in std_logic;
-			exc_stop	: in std_logic;
-			inh_or		: in std_logic;
-			inh_stop	: in std_logic;
-			input_spike	: in std_logic;
-
-			-- input from datapath
-			exceed_v_th	: in std_logic;
-
-			-- control output
-			update_sel	: out std_logic_vector(1 downto 0);
-			add_or_sub	: out std_logic;
-			v_update	: out std_logic;
-			v_en		: out std_logic;
-			v_rst_n		: out std_logic;
-
-			-- output
-			out_spike	: out std_logic;
-			neuron_ready	: out std_logic
-		);
-
-	end component neuron_cu;
-
-
 
 
 begin
 
+	-- allow ro upload v_th only in the load state
+	load_ready 	<= load_en;
+	masked_v_th_en	<= v_th_en and load_en;
 
-	datapath : neuron_datapath
+
+	-- mask the weight using the corresponding spike
+	spikes_masking	: process(exc_spike, exc_weight, inh_spike, inh_weight)
+	begin
+
+		bitwise_and : for i in 0 to weights_bit_width
+		loop
+			masked_exc_weight(i)	<= exc_weight(i) and exc_spike;
+			masked_inh_weight(i)	<= inh_weight(i) and inh_spike;
+
+		end loop bitwise_and;
+
+	end process spikes_masking;
+
+
+	control_unit	: neuron_cu
+		port map(
+			-- input
+			clk		=> clk,
+			rst_n		=> rst_n,
+			restart		=> restart,
+			load_end	=> load_end,
+			exc		=> exc,
+			inh		=> inh,
+
+			-- input from datapath
+			exceed_v_th	=> exceed_v_th,
+
+			-- control output
+			update_sel	=> update_sel,
+			add_or_sub	=> add_or_sub,
+			v_update	=> v_update,
+			v_en		=> v_en,
+			v_rst_n		=> v_rst_n,
+
+			-- output
+			load_ready	=> load_en,
+			neuron_ready	=> neuron_ready,
+			out_spike	=> out_spike
+		);
+
+
+	datapath	: neuron_datapath
 		generic map(
-			-- parallelism
+			-- bit-width
 			neuron_bit_width	=> neuron_bit_width,
 			weights_bit_width	=> weights_bit_width,
 
 			-- shift amount
 			shift			=> shift
 		)
-
+		
 		port map(
 			-- input parameters
-			v_th_value		=> v_th_value,	   	
-			v_reset			=> v_reset,	       
-			inh_weight		=> inh_weight,     
-			exc_weight		=> exc_weight,    
+			v_th_value		=> v_th_value,
+			v_reset			=> v_reset,
+			inh_weight		=> inh_weight,
+			exc_weight		=> exc_weight,
 
 			-- input controls
 			clk			=> clk,
-			update_sel		=> update_sel,		
-			add_or_sub		=> add_or_sub,	     
-			v_update		=> v_update,	     
-			v_th_en			=> v_th_en,	     
+			update_sel		=> update_sel,
+			add_or_sub		=> add_or_sub,
+			v_update		=> v_update,
+			v_th_en			=> masked_v_th_en,
 			v_en			=> v_en,
 			v_rst_n			=> v_rst_n,
-     
+
 			-- output
-			exceed_v_th		=> exceed_v_th
-		);
-
-
-	control_unit : neuron_cu
-		port map(
-			-- input
-			clk		=> clk,		
-			rst_n		=> rst_n,
-   			start		=> start,			
-			stop		=> stop,	     
-			exc_or		=> exc_or,	     
-			exc_stop	=> exc_stop,	     
-			inh_or		=> inh_or,	     
-			inh_stop	=> inh_stop,	     
-			input_spike	=> input_spike,	     
-
-			-- input from datapath
-			exceed_v_th	=> exceed_v_th,
-
-			-- control output
-			update_sel	=> update_sel,		
-			add_or_sub	=> add_or_sub,	     
-			v_update	=> v_update,	     
-			v_en		=> v_en,
-			v_rst_n		=> v_rst_n,
-                                                           
-			-- output
-			out_spike	=> out_spike,
-	   		neuron_ready	=> neuron_ready		
+			exceed_v_th		=> exceed_v_th		
 		);
 
 
