196cfa4ff0f2abe587aafa4d757b83f9cab0ced5
alessio <alessio.carpegna@polito.it>
2023-07-06 17:12:37 +0200

    First draft of automatic generation of a adder

---
+++ Python/VhdlGeneration/reg_signed.py
@@ -0,0 +1,78 @@
+import subprocess as sp
+
+import path_config
+
+from vhdl_block import VHDLblock
+
+class RegSigned(VHDLblock):
+
+	def __init__(self, default_bitwidth = 8, output_dir = "output"):
+
+		VHDLblock.__init__(self, entity_name = "reg_signed")
+
+		# Libraries and packages
+		self.library.add("ieee")
+		self.library["ieee"].package.add("std_logic_1164")
+		self.library["ieee"].package.add("numeric_std")
+
+		# Generics
+		self.entity.generic.add("N", "integer", str(default_bitwidth))
+
+		# Input ports
+		self.entity.port.add("clk", "in", "std_logic")
+		self.entity.port.add("en", "in", "std_logic")
+		self.entity.port.add("reg_in", "in", "signed(N-1 downto 0)")
+		
+		# Output ports
+		self.entity.port.add("reg_out", "out", "signed(N-1 downto 0)")
+
+		# Add/sub process
+		self.architecture.processes.add("sample")
+		self.architecture.processes[0].sensitivity_list.add("clk")
+		self.architecture.processes[0].sensitivity_list.add("en")
+
+		self.architecture.processes[0].if_list.add()
+		self.architecture.processes[0].if_list[0]._if_.conditions.add(
+				"clk'event")
+		self.architecture.processes[0].if_list[0]._if_.conditions.add(
+				"clk = '1'", "and")
+		self.architecture.processes[0].if_list[0]._if_.conditions.add(
+				"en = '1'", "and")
+
+		self.architecture.processes[0].if_list[0]._if_.body.add(
+				"reg_out <= reg_in;")
+
+
+		self.write_file(output_dir)
+
+
+	def compile(self, output_dir = "output"):
+
+		print("\nCompiling component %s\n"
+				%(self.entity.name))
+
+		command = "cd " + output_dir + "; "
+		command = command + "xvhdl " + self.entity.name + ".vhd" + "; "
+
+		sp.run(command, shell = True)
+
+		print("\n")
+	
+
+	def elaborate(self, output_dir = "output"):
+
+		print("\nElaborating component %s\n"
+				%(self.entity.name))
+
+		command = "cd " + output_dir + "; "
+		command = command + "xelab " + self.entity.name
+
+		sp.run(command, shell = True)
+
+		print("\n")
+
+
+reg_signed = RegSigned()
+
+reg_signed.compile()
+reg_signed.elaborate()
