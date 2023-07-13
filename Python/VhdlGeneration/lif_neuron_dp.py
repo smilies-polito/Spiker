ac2ea65429296638b930831aa5e68d7f3fb62cf0
alessio <alessio.carpegna@polito.it>
2023-07-13 16:17:29 +0200

    First draft of lif neuron generation.

---
+++ Python/VhdlGeneration/lif_neuron_dp.py
@@ -0,0 +1,144 @@
+import path_config
+from vhdl_block import VHDLblock
+from shifter import Shifter
+from mux4to1_signed import Mux4to1_signed
+from add_sub import AddSub
+from mux2to1_signed import Mux2to1_signed
+from reg_signed import RegSigned
+from reg_signed_sync_rst import RegSignedSyncRst
+from cmp_gt import CmpGt
+
+
+class LIFneuron(VHDLblock):
+
+	def __init__(self, default_bitwidth = 16, default_inh_weights_bitwidth =
+			5, default_exc_weights_bitwidth = 5,
+			default_shift = 10):
+
+		VHDLblock.__init__(self, entity_name = "neuron_datapath")
+
+		self.shifter			= Shifter(default_bitwidth =
+						default_bitwidth,
+						default_shift =
+						default_shift)
+
+		self.mux4to1_signed		= Mux4to1_signed(
+						default_bitwidth = 
+						default_bitwidth)
+
+		self.add_sub			= AddSub(
+						default_bitwidth = 
+						default_bitwidth)
+
+		self.mux2to1_signed		= Mux2to1_signed(
+						default_bitwidth = 
+						default_bitwidth)
+
+		self.reg_signed			= RegSigned(
+						default_bitwidth = 
+						default_bitwidth)
+
+		self.reg_signed_sync_rst	= RegSignedSyncRst(
+						default_bitwidth = 
+						default_bitwidth)
+
+		self.cmp_gt			= CmpGt(
+						default_bitwidth = 
+						default_bitwidth)
+
+		# Libraries and packages
+		self.library.add("ieee")
+		self.library["ieee"].package.add("std_logic_1164")
+		self.library["ieee"].package.add("numeric_std")
+				
+
+		# Generics
+		self.entity.generic.add(
+			name		= "neuron_bit_width", 
+			gen_type	= "integer",
+			value		= str(default_bitwidth))
+		self.entity.generic.add(
+			name		= "inh_weights_bit_width",
+			gen_type	= "integer",
+			value		= str(default_inh_weights_bitwidth))
+		self.entity.generic.add(
+			name		= "exc_weights_bit_width",
+			gen_type	= "integer",
+			value		= str(default_exc_weights_bitwidth))
+		self.entity.generic.add(
+			name		= "shift",
+			gen_type	= "integer",
+			value		= str(default_shift))
+
+		# Input parameters
+		self.entity.port.add(
+			name 		= "v_th_value", 
+			direction	= "in",
+			port_type	= "signed(neuron_bit_width-1 downto 0)")
+		self.entity.port.add(
+			name 		= "v_reset", 
+			direction	= "in",
+			port_type	= "signed(neuron_bit_width-1 downto 0)")
+		self.entity.port.add(
+			name 		= "inh_weight",
+			direction	= "in",
+			port_type	= "signed(inh_weights_bit_width-1 "
+						"downto 0)")
+		self.entity.port.add(
+			name 		= "exc_weight",
+			direction	= "in",
+			port_type	= "signed(exc_weights_bit_width-1 downto 0)")
+
+		# Input controls
+		self.entity.port.add(
+			name 		= "clk", 
+			direction	= "in", 
+			port_type	= "std_logic")
+		self.entity.port.add(
+			name 		= "update_sel",
+			direction	= "in", 
+			port_type	= "std_logic_vector(1 downto 0)")
+		self.entity.port.add(
+			name 		= "add_or_sub", 
+			direction	= "in", 
+			port_type	= "std_logic")
+		self.entity.port.add(
+			name 		= "v_update",
+			direction	= "in",
+			port_type	= "std_logic")
+		self.entity.port.add(
+			name 		= "v_th_en", 
+			direction	= "in",
+			port_type	= "std_logic")
+		self.entity.port.add(
+			name 		= "v_en",
+			direction	= "in",
+			port_type	= "std_logic")
+		self.entity.port.add(
+			name 		= "v_rst_n",
+			direction	= "in",
+			port_type	= "std_logic")
+
+
+		# Output
+		self.entity.port.add(
+			name 		= "exceed_v_th",
+			direction	= "out",
+			port_type	= "std_logic")
+		
+		self.entity.port.add(
+			name 		= "v_rst_n",
+			direction	= "in",
+			port_type	= "std_logic")
+
+		# Components
+		self.architecture.component.add(self.add_sub)
+		self.architecture.component.add(self.mux4to1_signed)
+		self.architecture.component.add(self.add_sub)
+		self.architecture.component.add(self.mux2to1_signed)
+		self.architecture.component.add(self.reg_signed)
+		self.architecture.component.add(self.reg_signed_sync_rst)
+		self.architecture.component.add(self.cmp_gt)
+
+a = LIFneuron()
+print(a.code())
