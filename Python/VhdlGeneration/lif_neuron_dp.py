e484cae355aaa4687a6df3fb1bfe7a2317a456f7
alessio <alessio.carpegna@polito.it>
2023-07-14 14:40:50 +0200

    First draft of LID neuron.
    I still miss to port map the mux4to1_signed, but I need to add a functionality
    to the port map function before

--- Python/VhdlGeneration/lif_neuron_dp.py
+++ Python/VhdlGeneration/lif_neuron_dp.py
@@ -1,3 +1,5 @@
+import subprocess as sp
+
 import path_config
 from vhdl_block import VHDLblock
 from shifter import Shifter
@@ -176,7 +178,7 @@ class LIFneuron(VHDLblock):
 		self.architecture.instances["v_shifter"].port_map("pos", "v",
 				"v_shifted")
 
-		# Adder port map
+		# Adder/subtractor port map
 		self.architecture.instances.add(self.add_sub, "update_add_sub")
 		self.architecture.instances["update_add_sub"].generic_map()
 		self.architecture.instances["update_add_sub"].g_map.add("N",
@@ -189,9 +191,111 @@ class LIFneuron(VHDLblock):
 		self.architecture.instances["update_add_sub"].p_map.add(
 				"add_sub_out", "update_value")
 
+		# Multiplexer 2 to 1 signed port map
+		self.architecture.instances.add(self.mux2to1_signed, "v_mux")
+		self.architecture.instances["v_mux"].generic_map()
+		self.architecture.instances["v_mux"].g_map.add("N",
+				"neuron_bit_width")
+		self.architecture.instances["v_mux"].port_map()
+		self.architecture.instances["v_mux"].p_map.add("sel",
+				"v_update")
+		self.architecture.instances["v_mux"].p_map.add("in0",
+				"v_reset")
+		self.architecture.instances["v_mux"].p_map.add("in1",
+				"update_value")
+		self.architecture.instances["v_mux"].p_map.add("mux_out",
+				"v_value")
 
 
+		# Signed register port map
+		self.architecture.instances.add(self.reg_signed, "v_th_reg")
+		self.architecture.instances["v_th_reg"].generic_map()
+		self.architecture.instances["v_th_reg"].g_map.add("N",
+				"neuron_bit_width")
+		self.architecture.instances["v_th_reg"].port_map()
+		self.architecture.instances["v_th_reg"].p_map.add("en",
+				"v_th_en")
+		self.architecture.instances["v_th_reg"].p_map.add("reg_in",
+				"v_th_value")
+		self.architecture.instances["v_th_reg"].p_map.add("reg_out",
+				"v_th")
 
 
-a = LIFneuron()
-print(a.code())
+		# Signed register with synchronous reset port map
+		self.architecture.instances.add(self.reg_signed_sync_rst,
+				"v_reg")
+		self.architecture.instances["v_reg"].generic_map()
+		self.architecture.instances["v_reg"].g_map.add("N",
+				"neuron_bit_width")
+		self.architecture.instances["v_reg"].port_map()
+		self.architecture.instances["v_reg"].p_map.add("en",
+				"v_en")
+		self.architecture.instances["v_reg"].p_map.add("rst_n",
+				"v_rst_n")
+		self.architecture.instances["v_reg"].p_map.add("reg_in",
+				"v_value")
+		self.architecture.instances["v_reg"].p_map.add("reg_out",
+				"v")
+
+		
+		# Signed comparator 
+		self.architecture.instances.add(self.cmp_gt,
+				"fire_cmp")
+		self.architecture.instances["fire_cmp"].generic_map()
+		self.architecture.instances["fire_cmp"].g_map.add("N",
+				"neuron_bit_width")
+		self.architecture.instances["fire_cmp"].port_map()
+		self.architecture.instances["fire_cmp"].p_map.add("in0",
+				"v_value")
+		self.architecture.instances["fire_cmp"].p_map.add("in1",
+				"v_th")
+		self.architecture.instances["fire_cmp"].p_map.add("cmp_out",
+				"exceed_v_th")
+		
+
+	def compile(self, output_dir = "output"):
+
+		print("\nCompiling component %s\n"
+				%(self.entity.name))
+
+		command = "cd " + output_dir + "; "
+		command = command + "xvhdl " + self.entity.name + ".vhd" + "; "
+
+		sp.run(command, shell = True)
+
+		print("\n")
+
+
+	def compile_all(self, output_dir = "output"):
+
+		self.shifter.compile()
+		self.add_sub.compile()
+		self.mux4to1_signed.compile()
+		self.add_sub.compile()
+		self.mux2to1_signed.compile()
+		self.reg_signed.compile()
+		self.reg_signed_sync_rst.compile()
+		self.cmp_gt.compile()
+
+		print("\nCompiling component %s\n"
+				%(self.entity.name))
+
+		command = "cd " + output_dir + "; "
+		command = command + "xvhdl " + self.entity.name + ".vhd" + "; "
+
+		sp.run(command, shell = True)
+
+		print("\n")
+
+
+	def elaborate(self, output_dir = "output"):
+
+		print("\nElaborating component %s\n"
+				%(self.entity.name))
+
+		command = "cd " + output_dir + "; "
+		command = command + "xelab " + self.entity.name
+
+		sp.run(command, shell = True)
+
+		print("\n")
