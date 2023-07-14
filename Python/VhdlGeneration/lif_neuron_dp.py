8685cb187f4a4104dc88e6bed5bfd529dcd834c7
alessio <alessio.carpegna@polito.it>
2023-07-14 16:04:31 +0200

    Move cmp input after the v register.
    New version of the neuron, still to test. It checks if the threshold has been
    exceeded after sampling the value of the membrane potential, not before

--- Python/VhdlGeneration/lif_neuron_dp.py
+++ Python/VhdlGeneration/lif_neuron_dp.py
@@ -211,79 +211,6 @@ class LIFneuronDP(VHDLblock):
 		self.architecture.instances["v_shifter"].port_map("pos", "v",
 				"v_shifted")
 
-		# Adder/subtractor
-		self.architecture.instances.add(self.add_sub, "update_add_sub")
-		self.architecture.instances["update_add_sub"].generic_map()
-		self.architecture.instances["update_add_sub"].g_map.add("N",
-				"neuron_bit_width")
-		self.architecture.instances["update_add_sub"].port_map()
-		self.architecture.instances["update_add_sub"].p_map.add("in0",
-				"v")
-		self.architecture.instances["update_add_sub"].p_map.add("in1",
-				"update")
-		self.architecture.instances["update_add_sub"].p_map.add(
-				"add_sub_out", "update_value")
-
-		# Multiplexer 2 to 1 signed
-		self.architecture.instances.add(self.mux2to1_signed, "v_mux")
-		self.architecture.instances["v_mux"].generic_map()
-		self.architecture.instances["v_mux"].g_map.add("N",
-				"neuron_bit_width")
-		self.architecture.instances["v_mux"].port_map()
-		self.architecture.instances["v_mux"].p_map.add("sel",
-				"v_update")
-		self.architecture.instances["v_mux"].p_map.add("in0",
-				"v_reset")
-		self.architecture.instances["v_mux"].p_map.add("in1",
-				"update_value")
-		self.architecture.instances["v_mux"].p_map.add("mux_out",
-				"v_value")
-
-
-		# Signed register
-		self.architecture.instances.add(self.reg_signed, "v_th_reg")
-		self.architecture.instances["v_th_reg"].generic_map()
-		self.architecture.instances["v_th_reg"].g_map.add("N",
-				"neuron_bit_width")
-		self.architecture.instances["v_th_reg"].port_map()
-		self.architecture.instances["v_th_reg"].p_map.add("en",
-				"v_th_en")
-		self.architecture.instances["v_th_reg"].p_map.add("reg_in",
-				"v_th_value")
-		self.architecture.instances["v_th_reg"].p_map.add("reg_out",
-				"v_th")
-
-
-		# Signed register with synchronous reset
-		self.architecture.instances.add(self.reg_signed_sync_rst,
-				"v_reg")
-		self.architecture.instances["v_reg"].generic_map()
-		self.architecture.instances["v_reg"].g_map.add("N",
-				"neuron_bit_width")
-		self.architecture.instances["v_reg"].port_map()
-		self.architecture.instances["v_reg"].p_map.add("en",
-				"v_en")
-		self.architecture.instances["v_reg"].p_map.add("rst_n",
-				"v_rst_n")
-		self.architecture.instances["v_reg"].p_map.add("reg_in",
-				"v_value")
-		self.architecture.instances["v_reg"].p_map.add("reg_out",
-				"v")
-
-		
-		# Signed comparator 
-		self.architecture.instances.add(self.cmp_gt,
-				"fire_cmp")
-		self.architecture.instances["fire_cmp"].generic_map()
-		self.architecture.instances["fire_cmp"].g_map.add("N",
-				"neuron_bit_width")
-		self.architecture.instances["fire_cmp"].port_map()
-		self.architecture.instances["fire_cmp"].p_map.add("in0",
-				"v_value")
-		self.architecture.instances["fire_cmp"].p_map.add("in1",
-				"v_th")
-		self.architecture.instances["fire_cmp"].p_map.add("cmp_out",
-				"exceed_v_th")
 
 		# Multiplexer 4 to 1 signed
 		self.architecture.instances.add(self.mux4to1_signed,
@@ -347,6 +274,83 @@ class LIFneuronDP(VHDLblock):
 		self.architecture.instances["update_mux"].p_map.add("mux_out",
 				"update")
 
+
+		# Adder/subtractor
+		self.architecture.instances.add(self.add_sub, "update_add_sub")
+		self.architecture.instances["update_add_sub"].generic_map()
+		self.architecture.instances["update_add_sub"].g_map.add("N",
+				"neuron_bit_width")
+		self.architecture.instances["update_add_sub"].port_map()
+		self.architecture.instances["update_add_sub"].p_map.add("in0",
+				"v")
+		self.architecture.instances["update_add_sub"].p_map.add("in1",
+				"update")
+		self.architecture.instances["update_add_sub"].p_map.add(
+				"add_sub_out", "update_value")
+
+
+		# Multiplexer 2 to 1 signed
+		self.architecture.instances.add(self.mux2to1_signed, "v_mux")
+		self.architecture.instances["v_mux"].generic_map()
+		self.architecture.instances["v_mux"].g_map.add("N",
+				"neuron_bit_width")
+		self.architecture.instances["v_mux"].port_map()
+		self.architecture.instances["v_mux"].p_map.add("sel",
+				"v_update")
+		self.architecture.instances["v_mux"].p_map.add("in0",
+				"v_reset")
+		self.architecture.instances["v_mux"].p_map.add("in1",
+				"update_value")
+		self.architecture.instances["v_mux"].p_map.add("mux_out",
+				"v_value")
+
+
+		# Signed register
+		self.architecture.instances.add(self.reg_signed, "v_th_reg")
+		self.architecture.instances["v_th_reg"].generic_map()
+		self.architecture.instances["v_th_reg"].g_map.add("N",
+				"neuron_bit_width")
+		self.architecture.instances["v_th_reg"].port_map()
+		self.architecture.instances["v_th_reg"].p_map.add("en",
+				"v_th_en")
+		self.architecture.instances["v_th_reg"].p_map.add("reg_in",
+				"v_th_value")
+		self.architecture.instances["v_th_reg"].p_map.add("reg_out",
+				"v_th")
+
+
+		# Signed register with synchronous reset
+		self.architecture.instances.add(self.reg_signed_sync_rst,
+				"v_reg")
+		self.architecture.instances["v_reg"].generic_map()
+		self.architecture.instances["v_reg"].g_map.add("N",
+				"neuron_bit_width")
+		self.architecture.instances["v_reg"].port_map()
+		self.architecture.instances["v_reg"].p_map.add("en",
+				"v_en")
+		self.architecture.instances["v_reg"].p_map.add("rst_n",
+				"v_rst_n")
+		self.architecture.instances["v_reg"].p_map.add("reg_in",
+				"v_value")
+		self.architecture.instances["v_reg"].p_map.add("reg_out",
+				"v")
+
+		
+		# Signed comparator 
+		self.architecture.instances.add(self.cmp_gt,
+				"fire_cmp")
+		self.architecture.instances["fire_cmp"].generic_map()
+		self.architecture.instances["fire_cmp"].g_map.add("N",
+				"neuron_bit_width")
+		self.architecture.instances["fire_cmp"].port_map()
+		self.architecture.instances["fire_cmp"].p_map.add("in0",
+				"v")
+		self.architecture.instances["fire_cmp"].p_map.add("in1",
+				"v_th")
+		self.architecture.instances["fire_cmp"].p_map.add("cmp_out",
+				"exceed_v_th")
+
+	
 		
 
 	def compile(self, output_dir = "output"):
