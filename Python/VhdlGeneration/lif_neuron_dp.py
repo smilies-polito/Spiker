01d80af4a1cd1aabbdeca20ae28e2ec771fb4c3a
alessio <alessio.carpegna@polito.it>
2023-07-14 15:34:20 +0200

    First draft of complete LIF neuron.

--- Python/VhdlGeneration/lif_neuron_dp.py
+++ Python/VhdlGeneration/lif_neuron_dp.py
@@ -170,7 +170,7 @@ class LIFneuron(VHDLblock):
 		self.architecture.component.add(self.cmp_gt)
 
 
-		# Shifter port map
+		# Shifter
 		self.architecture.instances.add(self.shifter, "v_shifter")
 		self.architecture.instances["v_shifter"].generic_map()
 		self.architecture.instances["v_shifter"].g_map.add("N",
@@ -178,7 +178,7 @@ class LIFneuron(VHDLblock):
 		self.architecture.instances["v_shifter"].port_map("pos", "v",
 				"v_shifted")
 
-		# Adder/subtractor port map
+		# Adder/subtractor
 		self.architecture.instances.add(self.add_sub, "update_add_sub")
 		self.architecture.instances["update_add_sub"].generic_map()
 		self.architecture.instances["update_add_sub"].g_map.add("N",
@@ -191,7 +191,7 @@ class LIFneuron(VHDLblock):
 		self.architecture.instances["update_add_sub"].p_map.add(
 				"add_sub_out", "update_value")
 
-		# Multiplexer 2 to 1 signed port map
+		# Multiplexer 2 to 1 signed
 		self.architecture.instances.add(self.mux2to1_signed, "v_mux")
 		self.architecture.instances["v_mux"].generic_map()
 		self.architecture.instances["v_mux"].g_map.add("N",
@@ -207,7 +207,7 @@ class LIFneuron(VHDLblock):
 				"v_value")
 
 
-		# Signed register port map
+		# Signed register
 		self.architecture.instances.add(self.reg_signed, "v_th_reg")
 		self.architecture.instances["v_th_reg"].generic_map()
 		self.architecture.instances["v_th_reg"].g_map.add("N",
@@ -221,7 +221,7 @@ class LIFneuron(VHDLblock):
 				"v_th")
 
 
-		# Signed register with synchronous reset port map
+		# Signed register with synchronous reset
 		self.architecture.instances.add(self.reg_signed_sync_rst,
 				"v_reg")
 		self.architecture.instances["v_reg"].generic_map()
@@ -251,6 +251,69 @@ class LIFneuron(VHDLblock):
 				"v_th")
 		self.architecture.instances["fire_cmp"].p_map.add("cmp_out",
 				"exceed_v_th")
+
+		# Multiplexer 4 to 1 signed
+		self.architecture.instances.add(self.mux4to1_signed,
+				"update_mux")
+		self.architecture.instances["update_mux"].generic_map()
+		self.architecture.instances["update_mux"].g_map.add("N",
+				"neuron_bit_width")
+		self.architecture.instances["update_mux"].port_map(mode = "no")
+		self.architecture.instances["update_mux"].p_map.add("sel",
+				"update_sel")
+		self.architecture.instances["update_mux"].p_map.add("in0",
+				"(others => '0')")
+		self.architecture.instances["update_mux"].p_map.add("in1",
+				"v_shifted")
+
+		if default_exc_weights_bitwidth < default_bitwidth:
+
+			self.architecture.instances["update_mux"].p_map.add(
+					"in2", "(others => "
+					"exc_weight(exc_weights_bit_width-1))", 
+					conn_range = "(neuron_bit_width-1 "
+					"downto exc_weights_bit_width)")
+			self.architecture.instances["update_mux"].p_map.add(
+					"in2", "exc_weight", 
+					conn_range = "(exc_weights_bit_width-1 "
+					"downto 0)")
+
+		elif default_exc_weights_bitwidth == default_bitwidth:
+			self.architecture.instances["update_mux"].p_map.add("in2",
+					"exc_weight")
+
+		else:
+			print("Excitatory weight bit-width cannot be larger "
+				"than the neuron's one")
+			exit(-1)
+
+
+		if default_inh_weights_bitwidth < default_bitwidth:
+
+			self.architecture.instances["update_mux"].p_map.add(
+					"in3", "(others => "
+					"inh_weight(inh_weights_bit_width-1))", 
+					conn_range = "(neuron_bit_width-1 "
+					"downto inh_weights_bit_width)")
+			self.architecture.instances["update_mux"].p_map.add(
+					"in3", "inh_weight", 
+					conn_range = "(inh_weights_bit_width-1 "
+					"downto 0)")
+
+		elif default_inh_weights_bitwidth == default_bitwidth:
+			self.architecture.instances["update_mux"].p_map.add("in3",
+					"inh_weight")
+
+		else:
+			print("Inhibitory weight bit-width cannot be larger "
+				"than the neuron's one")
+			exit(-1)
+
+
+		
+		self.architecture.instances["update_mux"].p_map.add("mux_out",
+				"update")
+
 		
 
 	def compile(self, output_dir = "output"):
@@ -259,7 +322,7 @@ class LIFneuron(VHDLblock):
 				%(self.entity.name))
 
 		command = "cd " + output_dir + "; "
-		command = command + "xvhdl " + self.entity.name + ".vhd" + "; "
+		command = command + "xvhdl --2008 " + self.entity.name + ".vhd"
 
 		sp.run(command, shell = True)
 
@@ -281,7 +344,7 @@ class LIFneuron(VHDLblock):
 				%(self.entity.name))
 
 		command = "cd " + output_dir + "; "
-		command = command + "xvhdl " + self.entity.name + ".vhd" + "; "
+		command = command + "xvhdl --2008 " + self.entity.name + ".vhd"
 
 		sp.run(command, shell = True)
 
@@ -299,3 +362,17 @@ class LIFneuron(VHDLblock):
 		sp.run(command, shell = True)
 
 		print("\n")
+
+
+	def write_file(self, output_dir = "output"):
+
+		self.shifter.write_file(output_dir = output_dir)
+		self.add_sub.write_file(output_dir = output_dir)
+		self.mux4to1_signed.write_file(output_dir = output_dir)
+		self.add_sub.write_file(output_dir = output_dir)
+		self.mux2to1_signed.write_file(output_dir = output_dir)
+		self.reg_signed.write_file(output_dir = output_dir)
+		self.reg_signed_sync_rst.write_file(output_dir = output_dir)
+		self.cmp_gt.write_file(output_dir = output_dir)
+
+		super().write_file(output_dir = output_dir)
