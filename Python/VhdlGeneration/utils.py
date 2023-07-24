f8bcb2a6061d09ade48ebc60fa7dfd49949e690c
alessio <alessio.carpegna@polito.it>
2023-07-24 13:10:36 +0200

    Function to track internal signals of component.

--- Python/VhdlGeneration/utils.py
+++ Python/VhdlGeneration/utils.py
@@ -24,3 +24,52 @@ def createDir(dirName):
 	
 	# Run the complete bash command
 	sp.run(cmdString, shell=True, executable="/bin/bash")
+
+
+def track_signals(signals_dict):
+
+	signals_list = list(signals_dict.keys())
+
+	exit_flag = 0
+	tracked = []
+	first = True
+	invalid_signal = False
+
+	while(exit_flag == 0):
+
+		while first == False and good_answ == False and \
+				invalid_signal == False:
+
+			answer = input("Do you want to add others?(y/n) ")
+
+			if answer == "n":
+
+				exit_flag = 1
+				good_answ = True
+
+			elif answer == "y":
+				good_answ = True
+
+		if exit_flag == 0:
+
+			input_msg = "Which signal do you want to track(write exit to " \
+				"stop)?\n\n" + str(signals_list) + "\n\n"
+			signal = input(input_msg)
+
+			if signal == "exit":
+				exit_flag = 1
+				invalid_signal = False
+				
+			elif signal not in signals_list:
+				print("\nInvalid answer. ")
+				invalid_signal = True
+
+			else:
+				tracked.append(signal)
+				signals_list.remove(signal)
+				invalid_signal = False
+
+			good_answ = False
+			first = False
+
+	return tracked
