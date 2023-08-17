23d0268162fcac641c0b9547f6992464c8fe8801
alessio <alessio.carpegna@polito.it>
2023-08-17 11:31:44 +0200

    Function to print the BRAM table.
    Table (python dictionary) containing all the possible combinations of sizes

--- Python/VhdlGeneration/bram.py
+++ Python/VhdlGeneration/bram.py
@@ -12,17 +12,23 @@ class Bram(VHDLblock):
 
 	def __init__(self, bram_size = "36Kb", device = "7series", do_reg = 0,
 			init_value = 0, init_file = "none", write_width = 72,
-			read_width = 72, srval = 0, write_mode = "write_first"):
+			read_width = 72, sim_collision_check = "all", srval = 0,
+			write_mode = "read_first"):
 
-		self.bram_kb = [18, 36]
-		self.bram_size_list = [str(size) + "Kb" for size in
-				self.bram_kb]
+		self.bram_tot_bit 	= [18432, 36864]
+		self.max_word_width 	= [36, 72]
+
+		self.bram_size_list = [str(int(size // 1000)) + "Kb" for size in
+				self.bram_tot_bit]
 
 		self.device_list = ["7series", "virtex5", "virtex6", 
 					"spartan6"]
-		self.max_word_width = 72
+
 		self.write_mode_list = ["write_first", "read_first", 
 					"no_change"]
+
+		self.sim_collision_check_list = ["all", "warning_only",
+				"generate_x_only", "none"]
 
 		self.init_width_bin = 256
 		self.init_width_hex = int(self.init_width_bin // 4)
@@ -39,26 +45,29 @@ class Bram(VHDLblock):
 			raise ValueError("Allowed Data Output Register "
 					"values are 0 and 1")
 
-		if init_value < 0 or init_value > self.max_word_width:
+		if init_value < 0 or init_value > self.max_word_width[-1]:
 			raise ValueError("Allowed init values for output "
 					"register are between 0 and " +
 					str(self.max_word_width) + " bit")
 
-		if write_width < 0 or bram_size == "18Kb" and write_width > \
-		int(self.max_word_width // 2) or bram_size == "36Kb" and \
-		write_width > int(self.max_word_width):
+		if write_width < 0 or write_width > \
+		self.max_word_width[self.bram_size_list.index(bram_size)]:
 			raise ValueError("Allowed Write Width between 1 "
 				"and 72. Values from 37 to 72 allowed only "
 				"if BRAM size is 36 Kb")
 
-		if read_width < 0 or bram_size == "18Kb" and read_width > \
-		int(self.max_word_width // 2) or bram_size == "36Kb" and \
-		read_width > int(self.max_word_width):
+		if read_width < 0 or read_width > \
+		self.max_word_width[self.bram_size_list.index(bram_size)]:
 			raise ValueError("Allowed Read Width between 1 "
 				"and 72. Values from 37 to 72 allowed only "
 				"if BRAM size is 36 Kb")
 
-		if srval < 0 or srval > self.max_word_width:
+		if sim_collision_check not in self.sim_collision_check_list:
+			raise ValueError("Valid values are ", 
+				self.sim_collision_check_list)
+
+
+		if srval < 0 or srval > self.max_word_width[-1]:
 			raise ValueError("Allowed set/reset values for output "
 					"register are between 0 and " +
 					str(self.max_word_width) + " bit")
@@ -67,8 +76,6 @@ class Bram(VHDLblock):
 			raise ValueError("Valid values are ", 
 				self.write_mode_list)
 
-
-		self.bram_depth = bram_size
 
 		# Target BRAM
 		self.bram_size	= bram_size
@@ -82,7 +89,7 @@ class Bram(VHDLblock):
 		# Initial values on the output port
 		self.init_value	= int_to_hex(
 				init_value, 
-				width = int(self.max_word_width // 4))
+				width = int(self.max_word_width[-1] // 4))
 
 		self.init_file = init_file
 
@@ -92,16 +99,16 @@ class Bram(VHDLblock):
 
 		self.srval = int_to_hex(
 				srval, 
-				width = int(self.max_word_width // 4))
+				width = int(self.max_word_width[-1] // 4))
 
 		self.write_mode = write_mode
 
 
 		width = max(self.read_width, self.write_width)
 
-		self.bram_depth = floor_pow2(self.bram_kb[
+		self.bram_depth = floor_pow2(self.bram_tot_bit[
 			self.bram_size_list.index(
-				self.bram_size)]*1000) // \
+				self.bram_size)]) // \
 			ceil_pow2(width - int(width // 8))
 
 		self.addr_width = int(log2(self.bram_depth))
@@ -109,7 +116,9 @@ class Bram(VHDLblock):
 		self.we_width = ceil_pow2(n_bytes(write_width - int(write_width
 			// 8)))
 
-		VHDLblock.__init__(self, "bram_single_macro")
+		self.sim_collision_check = sim_collision_check
+
+		VHDLblock.__init__(self, "bram_sdp_macro")
 
 		self.library.add("ieee")
 		self.library["ieee"].package.add("std_logic_1164")
@@ -131,7 +140,7 @@ class Bram(VHDLblock):
 		self.entity.generic.add(
 			name		= "init",
 			gen_type 	= "std_logic_vector(" +
-			str(self.max_word_width-1) + " downto 0)",
+			str(self.max_word_width[-1]-1) + " downto 0)",
 			value		= str("X\"" + self.init_value + "\""))
 		self.entity.generic.add(
 			name		= "init_file",
@@ -148,15 +157,19 @@ class Bram(VHDLblock):
 		self.entity.generic.add(
 			name		= "srval",
 			gen_type 	= "std_logic_vector(" +
-			str(self.max_word_width-1) + " downto 0)",
+			str(self.max_word_width[-1]-1) + " downto 0)",
 			value		= str("X\"" + self.srval + "\""))
 		self.entity.generic.add(
 			name 		= "write_mode",
 			gen_type 	= "string",
 			value		= "\"" + self.write_mode + "\"")
+		self.entity.generic.add(
+			name 		= "sim_collision_check",
+			gen_type 	= "string",
+			value		= "\"" + self.sim_collision_check + "\"")
 
 
-		for i in range(floor_pow2(self.bram_kb[-1]*1000) //
+		for i in range(floor_pow2(self.bram_tot_bit[-1]) //
 				self.init_width_bin):
 
 			self.entity.generic.add(
@@ -168,24 +181,22 @@ class Bram(VHDLblock):
 					self.init_width_hex*"0" + "\"")
 			)
 
-
-
 		self.entity.port.add(
 			name		= "do",
 			direction	= "out",
 			port_type	= "std_logic_vector(" +
 					str(self.read_width-1) + " downto 0)")
 		self.entity.port.add(
-			name		= "clk",
+			name		= "rdclk",
 			direction	= "in",
 			port_type	= "std_logic")
 		self.entity.port.add(
-			name		= "addr",
+			name		= "rdaddr",
 			direction	= "in",
 			port_type	= "std_logic_vector(" +
 					str(self.addr_width-1) + " downto 0)")
 		self.entity.port.add(
-			name		= "en",
+			name		= "rden",
 			direction	= "in",
 			port_type	= "std_logic")
 		self.entity.port.add(
@@ -202,184 +213,82 @@ class Bram(VHDLblock):
 			port_type	= "std_logic_vector(" +
 					str(self.we_width-1) + " downto 0)")
 		self.entity.port.add(
+			name		= "wrclk",
+			direction	= "in",
+			port_type	= "std_logic")
+		self.entity.port.add(
+			name		= "wraddr",
+			direction	= "in",
+			port_type	= "std_logic_vector(" +
+					str(self.addr_width-1) + " downto 0)")
+		self.entity.port.add(
+			name		= "wren",
+			direction	= "in",
+			port_type	= "std_logic")
+		self.entity.port.add(
 			name		= "di",
 			direction	= "in",
 			port_type	= "std_logic_vector(" +
 					str(write_width-1) + " downto 0)")
 
-	def compile(self, output_dir = "output"):
+	def shape_table(self):
 
-		print("\nCompiling component %s\n"
-				%(self.entity.name))
+		table = {}
 
-		command = "cd " + output_dir + "; "
-		command = command + "xvhdl --2008 " + self.entity.name + ".vhd"
+		for size in self.bram_size_list:
+			table[size] = {}
 
-		sp.run(command, shell = True)
+			table[size]["min_width"] = []
+			table[size]["max_width"] = []
+			table[size]["bram_depth"] = []
+			table[size]["addr_width"] = []
+			table[size]["we_width"] = []
 
-		print("\n")
+		for size, max_word_width in zip(self.bram_size_list,
+		self.max_word_width):
+			
+			count = 0
+			size_count = 0
 
-	def elaborate(self, output_dir = "output"):
-
-		print("\nElaborating component %s\n"
-				%(self.entity.name))
-
-		command = "cd " + output_dir + "; "
-		command = command + "xelab " + self.entity.name
-
-		sp.run(command, shell = True)
-
-		print("\n")
-
-	
-def comp(component, output_dir = "output"):
-
-	print("\nCompiling component %s\n"
-			%(component.entity.name))
-
-	command = "cd " + output_dir + "; "
-	command = command + "xvhdl --2008 " + component.entity.name + ".vhd"
-
-	sp.run(command, shell = True)
-
-	print("\n")
-
-def elaborate(component, output_dir = "output"):
-
-	print("\nElaborating component %s\n"
-			%(component.entity.name))
-
-	command = "cd " + output_dir + "; "
-	command = command + "xelab " + component.entity.name
-
-	sp.run(command, shell = True)
-
-	print("\n")
-	
+			for word_width in range(1, max_word_width+1):
 
 
-from testbench import Testbench
+				bram_depth = floor_pow2(self.bram_tot_bit[
+					self.bram_size_list.index(size)]) \
+					// ceil_pow2(word_width - 
+					int(word_width // 8))
 
-class prova(VHDLblock):
+				addr_width = int(
+					log2(bram_depth))
 
+				we_width = ceil_pow2(n_bytes(
+					word_width - int(word_width
+					// 8)))
+				
+				if not table[size]["bram_depth"] or \
+				bram_depth != table[size]["bram_depth"][-1]:
 
-	def __init__(self):
-		VHDLblock.__init__(self, "prova")
+					if table[size]["min_width"]:
+						table[size]["max_width"].append(
+						table[size][ "min_width"][-1] + 
+						count)
 
-	def compile(self, output_dir = "output"):
+					table[size]["min_width"].append(
+						word_width)
+					table[size]["bram_depth"].append(
+						bram_depth)
+					table[size]["addr_width"].append(
+						addr_width)
+					table[size]["we_width"].append(we_width)
+					
 
-		print("\nCompiling component %s\n"
-				%(self.entity.name))
+					count = 0
 
-		command = "cd " + output_dir + "; "
-		command = command + "xvhdl --2008 " + self.entity.name + ".vhd"
+				else:
+					count += 1
 
-		sp.run(command, shell = True)
+			if table[size]["min_width"]:
+				table[size]["max_width"].append(table[size]["min_width"][-1] + 
+					count)
 
-		print("\n")
-
-	def elaborate(self, output_dir = "output"):
-
-		print("\nElaborating component %s\n"
-				%(self.entity.name))
-
-		command = "cd " + output_dir + "; "
-		command = command + "xelab " + self.entity.name
-
-		sp.run(command, shell = True)
-
-		print("\n")
-
-a = prova()
-a.library.add("unisim")
-a.library["unisim"].package.add("vcomponents")
-a.library.add("unimacro")
-a.library["unimacro"].package.add("vcomponents")
-a.library.add("ieee")
-a.library["ieee"].package.add("std_logic_1164")
-
-b = Bram(
-	bram_size	= "36Kb",
-	device		= "7series",
-	do_reg		= 0,
-	init_value	= 0,
-	write_width	= 72,
-	read_width	= 72,
-	srval 		= 1
-
-)
-
-for key in b.entity.port:
-	a.entity.port.add(
-		name = b.entity.port[key].name,
-		direction = b.entity.port[key].direction,
-		port_type = b.entity.port[key].port_type
-	)
-
-a.architecture.constant.add(
-	name 		= "bram_size",
-	const_type	= "string",
-	value		= "\"36Kb\"")
-a.architecture.constant.add(
-	name 		= "device",
-	const_type	= "string",
-	value		= "\"7series\"")
-a.architecture.constant.add(
-	name 		= "do_reg",
-	const_type	= "integer",
-	value		= "0")
-a.architecture.constant.add(
-	name 		= "init",
-	const_type	= "bit_vector(71 downto 0)",
-	value		= "X\"" + 18*"0" + "\"")
-a.architecture.constant.add(
-	name 		= "init_file",
-	const_type	= "string",
-	value		= "\"none\"")
-a.architecture.constant.add(
-	name 		= "write_width",
-	const_type	= "integer",
-	value		= "72")
-a.architecture.constant.add(
-	name 		= "read_width",
-	const_type	= "integer",
-	value		= "72")
-a.architecture.constant.add(
-	name 		= "write_mode",
-	const_type	= "string",
-	value		= "\"write_first\"")
-a.architecture.constant.add(
-	name 		= "srval",
-	const_type	= "bit_vector(71 downto 0)",
-	value		= "X\"" + 18*"0" + "\"")
-
-a.architecture.instances.add(b, "bram")
-a.architecture.instances["bram"].port_map()
-a.architecture.instances["bram"].generic_map()
-a.architecture.instances["bram"].g_map.add("init_00",
-	"X\"00000000000000000000000000000000000000000000000000000000000000ff\"")
-
-for i in range(1, 128):
-	a.architecture.instances["bram"].g_map.add("init_" + int_to_hex(i, width
-		= 2), "X\"" + 64*"0"  + "\"")
-
-
-tb = Testbench(a)
-
-
-tb.architecture.processes["en_gen"].for_list.add("address", 0, 511)
-tb.architecture.processes["en_gen"].for_list[0].body.add("en <= \'1\';")
-tb.architecture.processes["en_gen"].for_list[0].body.add("addr <= "
-		"std_logic_vector(to_unsigned(i, 9));")
-tb.architecture.processes["en_gen"].for_list[0].body.add("wait for 20 ns;")
-tb.architecture.processes["en_gen"].for_list[0].body.add("en <= \'0\';")
-tb.architecture.processes["en_gen"].for_list[0].body.add(
-		"addr <= (others => '0');")
-tb.architecture.processes["en_gen"].for_list[0].body.add("wait for 20 ns;")
-
-print(tb.code())
-
-
-tb.write_file_all()
-tb.compile_all()
-tb.elaborate()
+		return table
