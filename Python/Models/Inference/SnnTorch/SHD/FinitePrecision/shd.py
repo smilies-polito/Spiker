2621e781d4dd33033a9f8d2b64c427fe70345ee0
alessio <alessio.carpegna@polito.it>
2023-10-03 18:08:05 +0200

    First draft of finite precision SHD.
    I have the code instrumented to work with the auntized network. I now need to
    run the same experiments I used for the MNIST.

--- Python/Models/Inference/SnnTorch/SHD/FinitePrecision/shd.py
+++ Python/Models/Inference/SnnTorch/SHD/FinitePrecision/shd.py
@@ -3,38 +3,20 @@ import tonic
 import numpy as np
 import torch
 from torch import nn
+from functools import partial
 
 class Quantize:
 
-	def fixedPoint(self, value, fp_dec, bitwidth):
-
-		quant = value * 2**fixed_point_decimals
-
-		if type(value).__module__ == np.__name__:
-			quant = quant.astype(int)
-
-		elif type(value).__module__ == torch.__name__:
-			quant = quant.int()
-
-		else:
-			quant = int(quant)
-
-
-		return saturate(quant, bitwidth)
-
-	def saturate(value, bitwidth):
-
+	def saturate(self, value, bitwidth):
 
 		if type(value).__module__ == np.__name__ or \
 		type(value).__module__ == torch.__name__:
 
 			value[value > 2**(bitwidth-1)-1] = \
-				int(2**(bitwidth-1)-1)
+				2**(bitwidth-1)-1
 			value[value < -2**(bitwidth-1)] = \
-				int(-2**(bitwidth-1))
-
+				-2**(bitwidth-1)
 		else:
-
 			if value > 2**(bitwidth-1)-1:
 				value = 2**(bitwidth-1)-1
 
@@ -42,6 +24,28 @@ class Quantize:
 				value = -2**(bitwidth-1)
 
 		return value
+
+	def to_int(self, value):
+
+		if type(value).__module__ == np.__name__:
+			quant = value.astype(int)
+
+		elif type(value).__module__ == torch.__name__:
+			quant = value.type(torch.int64)
+
+		else:
+			quant = int(value)
+
+		return quant
+
+	def saturated_int(self, value, bitwidth):
+		return self.saturate(self.to_int(value), bitwidth)
+
+	def fixed_point(self, value, fp_dec, bitwidth):
+
+		quant = value * 2**fp_dec
+
+		return self.to_int(self.saturate(quant, bitwidth))
 
 
 class TonicTransform:
@@ -85,34 +89,38 @@ class TonicTransform:
 
 class LIF:
 
-	def __init__(self, alpha, beta, n_neurons, threshold = 1.0, 
+	def __init__(self, alpha_shift, beta_shift, n_neurons, threshold = 1.0, 
 	readout = False):
-		self.n_neurons	= n_neurons
-		self.syn 	= torch.zeros(n_neurons)
-		self.alpha	= alpha
-		self.mem 	= torch.zeros(n_neurons)
-		self.beta	= beta
-		self.th		= threshold
-		self.out	= torch.zeros(n_neurons)
-		self.readout	= readout
 
-		self.quant	= Quantize()
+		self.quant		= Quantize()
+
+		self.n_neurons		= n_neurons
+		self.syn		= self.quant.to_int(torch.zeros(
+						n_neurons))
+		self.alpha_shift	= alpha_shift
+		self.mem		= self.quant.to_int(torch.zeros(
+						n_neurons))
+		self.beta_shift		= beta_shift
+		self.th			= threshold
+		self.out		= self.quant.to_int(torch.zeros(
+						n_neurons))
+		self.readout		= readout
 
 	def init_neurons(self):
-		self.mem = torch.zeros(self.n_neurons)
-		self.syn = torch.zeros(self.n_neurons)
-		self.out = torch.zeros(self.n_neurons)
+		self.mem = self.quant.to_int(torch.zeros(self.n_neurons))
+		self.syn = self.quant.to_int(torch.zeros(self.n_neurons))
+		self.out = self.quant.to_int(torch.zeros(self.n_neurons))
 
-	def exp_decay(self, value, decay_rate):
-		return value - value*decay_shift
+	def exp_decay(self, value, decay_shift):
+		return self.quant.to_int(value - value*2**(-decay_shift))
 
 	def reset(self, value):
 		rst = self.out.detach()
-		value[rst == 1] = 0
+		value[rst == 1] = value[rst == 1] - self.th
 
 	def spike_fn(self, mthr):
 		self.out = torch.zeros_like(mthr)
-		self.out[mthr > 0] = 1.0
+		self.out[mthr > 0] = 1
 
 
 	def __call__(self, input_current):
@@ -120,20 +128,25 @@ class LIF:
 		if not self.readout:
 			self.reset(self.mem)
 
-		mthr = self.mem - self.th
+		mthr = self.quant.to_int(self.mem - self.th)
 
 		self.spike_fn(mthr)
 
-		self.mem = self.exp_decay(self.mem, self.beta) + self.syn
-		self.syn = self.exp_decay(self.syn, self.alpha) + input_current
+		self.mem = self.quant.to_int(self.exp_decay(self.mem,
+			self.beta_shift) + self.quant.to_int(self.syn))
 
-		return self.out, self.mem
+		self.syn = self.quant.to_int(self.exp_decay(self.syn,
+			self.alpha_shift) + self.quant.to_int(input_current))
+
+		return self.quant.to_int(self.out), self.quant.to_int(self.mem)
 
 
 class SNN(nn.Module):
 
-	def __init__(self, num_inputs, num_hidden, num_output, alpha, beta,
-			threshold = 1.0, alpha2 = None, beta2 = None):
+	def __init__(self, num_inputs, num_hidden, num_output, alpha_shift,
+			beta_shift, w1, v1, w2, bitwidth1 = 64, bitwidth2 = 64,
+			threshold = 1.0, alpha_shift2 = None, beta_shift2 =
+			None):
 
 		super().__init__()
 
@@ -141,26 +154,44 @@ class SNN(nn.Module):
 		self.num_hidden = num_hidden
 		self.num_output = num_output
 
-		if not alpha2:
-			alpha2 = alpha
+		self.bitwidth1 = bitwidth1
+		self.bitwidth2 = bitwidth2
 
-		if not beta2:
-			beta2 = beta
+		self.w1 = w1
+		self.v1 = v1
+		self.w2 = w2
+
+		print(self.w1)
+		print(self.v1)
+		print(self.w2)
+
+		if not alpha_shift2:
+			alpha_shift2 = alpha_shift
+
+		if not beta_shift2:
+			beta_shift2 = beta_shift
+
+		if type(alpha_shift) != int or type(beta_shift) != int or \
+		type(alpha_shift2) !=  int or type(beta_shift2) != int:
+			raise ValueError("Invalid exponential shift value")
+
+		self.quant = Quantize()
 
 		# Input fully connected layer
-		self.fc_in = nn.Linear(num_inputs, num_hidden, bias = False)
+		self.fc_in = partial(torch.einsum, "ab, b -> a")
 
 		# Feedback fully connected layer
-		self.fc_fb = nn.Linear(num_hidden, num_hidden, bias = False)
+		self.fc_fb = partial(torch.einsum, "ab, b -> a")
 
 		# Hidden layer of LIF neuron
-		self.lif1 = LIF(alpha, beta, num_hidden, threshold)
+		self.lif1 = LIF(alpha_shift, beta_shift, num_hidden, threshold)
 
 		# Readout fully connected layer
-		self.fc_ro = nn.Linear(num_hidden, num_output, bias = False)
+		self.fc_ro = partial(torch.einsum, "ab, b -> a")
 
 		# Readout layer
-		self.readout = LIF(alpha2, beta2, num_output, readout = True)
+		self.readout = LIF(alpha_shift2, beta_shift2, num_output,
+				readout = True)
 
 	def forward(self, input_spikes, n_steps = None):
 
@@ -171,8 +202,8 @@ class SNN(nn.Module):
 			raise ValueError("Incompatible number of steps for "
 			"input")
 
-		hidden_spikes = torch.zeros(self.num_hidden)
-		ro_mem = torch.zeros(self.num_output)
+		hidden_spikes = self.quant.to_int(torch.zeros(self.num_hidden))
+		ro_mem = self.quant.to_int(torch.zeros(self.num_output))
 
 		ro_mem_rec = []
 
@@ -181,13 +212,22 @@ class SNN(nn.Module):
 
 		for i in range(n_steps):
 
-			in_syn_curr = self.fc_in(input_spikes[i])
-			fb_syn_curr = self.fc_fb(hidden_spikes)
-			syn_curr = in_syn_curr + fb_syn_curr
+			in_syn_curr = self.quant.saturated_int(
+					self.fc_in((self.w1.float(),
+						input_spikes[i].float())),
+					self.bitwidth1)
+			fb_syn_curr = self.quant.saturated_int(
+					self.fc_fb((self.v1.float(),
+						hidden_spikes.float())),
+					self.bitwidth1)
+			syn_curr = self.quant.saturated_int(in_syn_curr +
+					fb_syn_curr, self.bitwidth1)
 
 			hidden_spikes, _ = self.lif1(syn_curr)
 
-			ro_syn_curr = self.fc_ro(hidden_spikes)
+			ro_syn_curr = self.quant.saturated_int(self.fc_ro((
+				self.w2.float(), hidden_spikes.float())), self.bitwidth2)
+
 			_, ro_mem = self.readout(ro_syn_curr)
 
 			ro_mem_rec.append(ro_mem)
@@ -221,7 +261,7 @@ def compute_classification_accuracy(dataset):
 
 
 
-
+quantize = Quantize()
 
 # Check whether a GPU is available
 if torch.cuda.is_available():
@@ -243,8 +283,16 @@ time_step = 1e-3
 tau_mem = 10e-3
 tau_syn = 5e-3
 
-alpha   = float(np.exp(-time_step/tau_syn))
-beta    = float(np.exp(-time_step/tau_mem))
+alpha_shift = 4
+beta_shift = 3
+
+fp_dec = 10
+bitwidth1 = 16
+bitwidth2 = 16
+w_bitwidth1 = 6
+w_bitwidth_fb1 = 6
+w_bitwidth2 = 6
+threshold = quantize.fixed_point(1, fp_dec, bitwidth1)
 
 dataset = tonic.datasets.hsd.SHD(save_to='./data', train=False)
 
@@ -255,19 +303,29 @@ transform = TonicTransform(
 	n_inputs	= n_inputs
 )
 
-snn = SNN(
-	num_inputs = n_inputs,
-	num_hidden = n_hidden,
-	num_output = n_output,
-	alpha = alpha,
-	beta = beta,
-)
 
-snn.fc_in.weight.data = torch.load("w1.pt",
-		map_location=torch.device('cpu')).transpose(0, 1)
-snn.fc_fb.weight.data = torch.load("v1.pt",
-		map_location=torch.device('cpu')).transpose(0, 1)
-snn.fc_ro.weight.data = torch.load("w2.pt",
-		map_location=torch.device('cpu')).transpose(0, 1)
+w1 = quantize.fixed_point(torch.load("w1.pt",
+		map_location=torch.device('cpu')).transpose(0, 1), fp_dec,
+		w_bitwidth1)
+v1 = quantize.fixed_point(torch.load("v1.pt",
+		map_location=torch.device('cpu')).transpose(0, 1), fp_dec,
+		w_bitwidth_fb1)
+w2 = quantize.fixed_point(torch.load("w2.pt",
+		map_location=torch.device('cpu')).transpose(0, 1), fp_dec,
+		w_bitwidth2)
+
+snn = SNN(
+	num_inputs	= n_inputs,
+	num_hidden	= n_hidden,
+	num_output	= n_output,
+	threshold	= threshold,
+	alpha_shift	= alpha_shift,
+	beta_shift	= beta_shift,
+	w1		= w1,
+	v1		= v1,
+	w2		= w2,
+	bitwidth1	= bitwidth1,
+	bitwidth2	= bitwidth2
+)
 
 print(compute_classification_accuracy(dataset))
