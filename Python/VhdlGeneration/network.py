223e420e62da8373571779f4f20d169cb1827ad1
alessio <alessio.carpegna@polito.it>
2023-10-17 15:49:09 +0200

    Complete dummy accelerator + testbench

--- Python/VhdlGeneration/network.py
+++ Python/VhdlGeneration/network.py
@@ -16,6 +16,8 @@ import path_config
 from vhdl_block import VHDLblock
 from if_statement import If, ConditionsList
 from text import SingleCodeLine
+from for_statement import For
+from instance import Instance
 
 
 class Network(VHDLblock, dict):
@@ -23,6 +25,7 @@ class Network(VHDLblock, dict):
 	def __init__(self, n_cycles = 10, debug = False, debug_list = []):
 
 		self.layer_index 	= 0
+		self.line_pointer	= 0
 		self.all_ready 		= ConditionsList()
 		self.n_cycles		= n_cycles
 
@@ -83,9 +86,19 @@ class Network(VHDLblock, dict):
 			direction	= "in",
 			port_type	= "std_logic")
 
+		self.entity.port.add(
+			name 		= "sample_ready", 
+			direction	= "in",
+			port_type	= "std_logic")
+
 		# Output
 		self.entity.port.add(
 			name 		= "ready", 
+			direction	= "out",
+			port_type	= "std_logic")
+
+		self.entity.port.add(
+			name 		= "sample", 
 			direction	= "out",
 			port_type	= "std_logic")
 
@@ -108,11 +121,21 @@ class Network(VHDLblock, dict):
 			signal_type	= "std_logic"
 		)
 
+		self.architecture.bodyCodeHeader.add(
+			"sample <= start_all;"
+		)
+		self.line_pointer += 1
+
 		# Multi-input control
 		self.architecture.instances.add(self.multi_cycle,
 				"multi_cycle_control")
 		self.architecture.instances["multi_cycle_control"].generic_map()
 		self.architecture.instances["multi_cycle_control"].port_map()
+
+		self.all_ready.add("sample_ready")
+		self.architecture.bodyCodeHeader.add("all_ready <= " +
+				self.all_ready.code() + ";\n")
+		self.line_pointer += 1
 
 		
 		# Debug
@@ -184,10 +207,6 @@ class Network(VHDLblock, dict):
 			self.architecture.instances[current_layer].p_map.add(
 					"exc_spikes", "in_spikes")
 
-			self.all_ready.add(layer_ready)
-			self.architecture.bodyCodeHeader.add("all_ready <= " +
-					self.all_ready.code() + ";\n")
-
 			self.architecture.bodyCodeHeader.add("out_spikes <= ",
 					current_layer + "_feedback;\n")
 
@@ -221,10 +240,6 @@ class Network(VHDLblock, dict):
 			self.architecture.instances[current_layer].p_map.add(
 				"exc_spikes", exc_spikes_internal)
 
-			self.all_ready.add(layer_ready, "and")
-			self.architecture.bodyCodeHeader[0] = SingleCodeLine(
-					"all_ready <= " + self.all_ready.code()
-					+ ";\n")
 
 			self.architecture.bodyCodeHeader[self.layer_index] = \
 				SingleCodeLine(exc_spikes_internal + "<= ",
@@ -232,6 +247,11 @@ class Network(VHDLblock, dict):
 
 			self.architecture.bodyCodeHeader.add("out_spikes <= ",
 					current_layer + "_feedback;\n")
+
+		self.all_ready.add(layer_ready, "and")
+		self.architecture.bodyCodeHeader[self.line_pointer-1] = SingleCodeLine(
+				"all_ready <= " + self.all_ready.code()
+				+ ";\n")
 
 
 
@@ -330,16 +350,18 @@ class Network_tb(Testbench):
 		self.architecture.processes["start_gen"].if_list[0]._if_.\
 			body.add(ready_if)
 
-		if file_input and "in_spikes" in input_signal_list:
+		if file_input and "in_spike" in input_signal_list:
 			del self.architecture.processes["in_spikes_rd_en_gen"]
 			self.architecture.bodyCodeHeader.add(
-				"in_spikes_rd_en <= ready;")
+				"in_spike_rd_en <= ready;")
 			self.architecture.bodyCodeHeader.add(
-				"out_spikes_w_en <= ready;")
+				"out_spike_w_en <= ready;")
 
 class DummyAccelerator(VHDLblock):
 
 	def __init__(self, config, debug = False, debug_list = []):
+
+		self.name = "dummy_spiker"
 
 		self.net = Network(
 				n_cycles	= config["n_cycles"],
@@ -348,6 +370,13 @@ class DummyAccelerator(VHDLblock):
 		)
 
 		self.layer_sizes = []
+
+		if "layer_0" in config.keys():
+			if "w_exc" in config["layer_0"].keys():
+				self.input_size = config["layer_0"]["w_exc"].\
+							shape[1]
+		else:
+			raise ValueError("Invalid config dictionary")
 
 		for key in config:
 			if "layer" in key:
@@ -374,21 +403,21 @@ class DummyAccelerator(VHDLblock):
 								"debug_list"]
 				)
 
+				self.net.add(new_layer)
+
 				self.layer_sizes.append(
 					config[key]["w_exc"].shape[0]
 				)
 
-			print(self.layer_sizes)
+		self.in_addr_bw	= int(log2(ceil_pow2(self.layer_sizes[0])))
+		self.out_addr_bw = int(log2(ceil_pow2(self.layer_sizes[-1])))
 
 		self.input_decoder = Decoder(
-			bitwidth = int(log2(ceil_pow2(self.layer_sizes[0])))
+			bitwidth = self.in_addr_bw
 		)
 
-		print(int(log2(ceil_pow2(self.layer_sizes[-1]))))
-
 		self.output_mux = Mux(
-			n_in		= int(log2(ceil_pow2(
-					self.layer_sizes[-1]))),
+			n_in		= 2**self.out_addr_bw,
 			in_type		= "std_logic",
 			bitwidth	= 1,
 		)
@@ -398,6 +427,238 @@ class DummyAccelerator(VHDLblock):
 			reg_type	= "std_logic",
 		)
 
+		self.components = sub_components(self)
+
+		super().__init__(entity_name = self.name)
+		self.vhdl(debug = debug, debug_list = debug_list)
+
+	def vhdl(self, debug = False, debug_list = []):
+
+		# Libraries and packages
+		self.library.add("ieee")
+		self.library["ieee"].package.add("std_logic_1164")
+
+		self.library.add("work")
+		self.library["work"].package.add("spiker_pkg")
+
+		for name in self.net.entity.generic:
+			self.entity.generic.add(
+				name		= name,
+				gen_type	= self.net.entity.generic[
+						name].gen_type,
+				value		= self.net.entity.generic[
+						name].value
+			)	
+
+		for name in self.net.entity.port:
+			if name != "in_spikes" and name != \
+			"out_spikes":
+
+				self.entity.port.add(
+					name		= name,
+					direction	= self.net.entity.port[
+							name].direction,
+					port_type	= self.net.entity.port[
+							name].port_type
+				)	
+
+		self.entity.port.add(
+			name		= "in_spike",
+			direction	= "in",
+			port_type	= "std_logic"
+		)	
+
+		self.entity.port.add(
+			name		= "in_spike_addr",
+			direction	= "in",
+			port_type	= "std_logic_vector(" + 
+					str(self.in_addr_bw - 1) + " downto 0)"
+		)	
+
+		self.entity.port.add(
+			name		= "out_spike",
+			direction	= "out",
+			port_type	= "std_logic"
+		)	
+
+		self.entity.port.add(
+			name		= "out_spike_addr",
+			direction	= "in",
+			port_type	= "std_logic_vector(" + 
+					str(self.out_addr_bw - 1) + " downto 0)"
+		)	
+
+		self.architecture.component.add(self.net)
+		self.architecture.component.add(self.input_decoder)
+		self.architecture.component.add(self.output_mux)
+		self.architecture.component.add(self.ff)
+
+		self.architecture.signal.add(
+			name		= "en",
+			signal_type	= "std_logic_vector(" +
+					str(2**self.in_addr_bw-1)
+					+ " downto 0)"
+		)
+
+		self.architecture.signal.add(
+			name		= "in_spikes",
+			signal_type	= "std_logic_vector(" +
+					str(2**self.in_addr_bw-1)
+					+ " downto 0)"
+		)
+
+		self.architecture.signal.add(
+			name		= "out_spikes",
+			signal_type	= "std_logic_vector(" +
+					str(self.layer_sizes[-1]-1)
+					+ " downto 0)"
+		)
+
+		ff_instance = Instance(self.ff, "spike_reg_i")
+		ff_instance.port_map("key", **{
+			"clk"		: "clk",
+			"reg_in"	: "in_spike",
+			"en"		: "en(i)",
+			"reg_out"	: "in_spikes(i)"})
+
+		spikes_sample = For(
+			name		= "spikes",
+			start		= 0,
+			stop		= self.input_size-1,
+			loop_type	= "generate"
+		)
+
+		spikes_sample.body.add(ff_instance)
+
+		self.architecture.bodyCodeHeader.add(spikes_sample)
+
+		self.architecture.instances.add(self.input_decoder,
+				"input_decoder")
+		self.architecture.instances["input_decoder"].generic_map("key",
+			**{"bitwidth"	: str(self.in_addr_bw)})
+		self.architecture.instances["input_decoder"].port_map("key", **{
+			"encoded_in"	: "in_spike_addr",
+			"decoded_out"	: "en"}
+		)
+
+		self.architecture.instances.add(self.output_mux,
+				"output_mux")
+		self.architecture.instances["output_mux"].port_map()
+
+		self.architecture.instances["output_mux"].p_map.add(
+			"mux_sel", "out_spike_addr"
+		)
+
+		for i in range(self.layer_sizes[-1]):
+			self.architecture.instances["output_mux"].p_map.add(
+				"in" + str(i), "out_spikes(" + str(i) + ")"
+			)
+
+		if self.layer_sizes[-1] < 2**self.out_addr_bw:
+			for i in range(self.layer_sizes[-1],
+			2**self.out_addr_bw):
+				self.architecture.instances["output_mux"].p_map.add(
+					"in" + str(i), "\'0\'"
+				)
+
+
+		self.architecture.instances["output_mux"].p_map.add(
+			"mux_out", "out_spike"
+		)
+
+		self.architecture.instances.add(self.net,
+				"snn")
+		self.architecture.instances["snn"].generic_map()
+		self.architecture.instances["snn"].port_map()
+
+
+class DummyAccelerator_tb(Testbench):
+
+	def __init__(self, dummy_accelerator, clock_period = 20, file_output =
+			False, output_dir = "output", file_input = False,
+			input_dir = "", input_signal_list = [], debug = False,
+			debug_list = []):
+
+
+		self.spiker_pkg = SpikerPackage()
+
+		self.dut = dummy_accelerator
+		self.components = sub_components(self)
+
+		super().__init__(
+			dut = self.dut, 
+			clock_period = clock_period,
+			file_output = file_output,
+			output_dir = output_dir,
+			file_input = file_input,
+			input_dir = input_dir,
+			input_signal_list = input_signal_list
+		)
+		
+		self.vhdl(
+			clock_period		= clock_period,
+			file_output		= file_output,
+			output_dir		= output_dir,
+			file_input		= file_input,
+			input_dir		= input_dir,
+			input_signal_list 	= input_signal_list
+			)
+
+
+	def vhdl(self, clock_period = 20, file_output = False, output_dir =
+			"output", file_input = False, input_dir = "",
+			input_signal_list = []):
+
+		self.library.add("work")
+		self.library["work"].package.add("spiker_pkg")
+
+		# rst_n
+		self.architecture.processes["rst_n_gen"].bodyHeader.add(
+				"rst_n <= '1';")
+		self.architecture.processes["rst_n_gen"].bodyHeader.add(
+				"wait for 15 ns;")
+		self.architecture.processes["rst_n_gen"].bodyHeader.add(
+				"rst_n <= '0';")
+		self.architecture.processes["rst_n_gen"].bodyHeader.add(
+				"wait for 10 ns;")
+		self.architecture.processes["rst_n_gen"].bodyHeader.add(
+				"rst_n <= '1';")
+
+		# Start
+		ready_if = If()
+		ready_if._if_.conditions.add("ready = '1'")
+		ready_if._if_.body.add("start <= '1';")
+		ready_if._else_.body.add("start <= '0';")
+
+		self.architecture.processes["start_gen"].final_wait = False
+		self.architecture.processes["start_gen"].sensitivity_list.\
+			add("clk")
+		self.architecture.processes["start_gen"].if_list.add()
+		self.architecture.processes["start_gen"].if_list[0]._if_.\
+			conditions.add("clk'event")
+		self.architecture.processes["start_gen"].if_list[0]._if_.\
+			conditions.add("clk = '1'", "and")
+		self.architecture.processes["start_gen"].if_list[0]._if_.\
+			body.add(ready_if)
+
+		if file_input and "in_spikes" in input_signal_list:
+			del self.architecture.processes["in_spikes_rd_en_gen"]
+
+			self.architecture.bodyCodeHeader.add(
+				"in_spikes_rd_en <= sample;")
+			self.architecture.bodyCodeHeader.add(
+				"out_spikes_w_en <= sample;")
+
+			self.architecture.processes["in_spikes_gen"].bodyHeader.add(
+				"sample_ready <= '0';")
+
+		del self.architecture.processes["sample_ready_gen"]
+		self.architecture.bodyCodeHeader.add("sample_ready <= sample;")
+
+			
+
+
+from vhdl import fast_compile, write_file_all, elaborate
 config = {}
 
 config["n_cycles"] = 10
@@ -429,4 +690,12 @@ config["layer_0"] = {
 
 a = DummyAccelerator(config)
 
-print(a.code())
+a_tb = DummyAccelerator_tb(a,
+	file_input = True,
+	input_signal_list = ["in_spike"]
+)
+
+write_file_all(a_tb)
+
+fast_compile(a_tb)
+elaborate(a_tb)
