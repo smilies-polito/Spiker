402fa13c4ee5a0b2124ebb1e7cad5c3d764a0623
alessio <alessio.carpegna@polito.it>
2023-10-18 10:49:17 +0200

    Correct include paths

--- Python/Simulations/PythonSim/VhdlGeneration/path_config.py
+++ Python/Simulations/PythonSim/VhdlGeneration/path_config.py
@@ -1,10 +1,11 @@
 import sys
 
 vhdl_gen_dir = "../../../VhdlGeneration"
-hdl_dir = "../../../HdlTools"
-
-if hdl_dir not in sys.path:
-	sys.path.append(hdl_dir)
+hdl_dir = "../../../HdlTools/hdltools"
 
 if vhdl_gen_dir not in sys.path:
-	sys.path.append(vhdl_gen_dir)
+	sys.path.insert(0, vhdl_gen_dir)
+
+if hdl_dir not in sys.path:
+	sys.path.insert(0, hdl_dir)
+
