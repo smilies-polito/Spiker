51f135c3b0e00ae3f148e1c076e537a6687de02b
alessio <alessio.carpegna@polito.it>
2023-10-24 10:28:54 +0200

    Generate spikes with the desired amount of 1s

---
+++ Python/Simulations/PythonSim/VhdlGeneration/spikes_gen.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+np.set_printoptions(threshold = np.inf)
+
+target_dir = "./MNIST_spiker"
+spikes_file = "in_spike.txt"
+
+spikes_file = target_dir + "/" + spikes_file
+
+p0 = 0.5
+p1 = 1 - p0
+
+spikes = np.random.choice([0, 1], size = (1000, 784), p = [p0, p1])
+
+with open(spikes_file, "w") as fp:
+	for t in range(spikes.shape[0]):
+		s = str(spikes[t])[1:-1].replace(" ", "").replace("\n", "")
+
+		fp.write(s)
+		fp.write("\n")
