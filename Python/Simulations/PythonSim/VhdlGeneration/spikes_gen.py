29545f0c0f07eff898fa3e05ce351b795085c799
alessio <alessio.carpegna@polito.it>
2023-11-30 14:38:42 +0100

    Generate dummy inputs for SHD model

--- Python/Simulations/PythonSim/VhdlGeneration/spikes_gen.py
+++ Python/Simulations/PythonSim/VhdlGeneration/spikes_gen.py
@@ -2,15 +2,15 @@ import numpy as np
 
 np.set_printoptions(threshold = np.inf)
 
-target_dir = "./MNIST_spiker"
+target_dir = "./DummyAccelerator"
 spikes_file = "in_spike.txt"
 
 spikes_file = target_dir + "/" + spikes_file
 
-p0 = 0.5
+p0 = 0
 p1 = 1 - p0
 
-spikes = np.random.choice([0, 1], size = (1000, 784), p = [p0, p1])
+spikes = np.random.choice([0, 1], size = (1000, 700), p = [p0, p1])
 
 with open(spikes_file, "w") as fp:
 	for t in range(spikes.shape[0]):
