6b9faf4d32269d6483c2b8025121e393fc259136
alessio <alessio.carpegna@polito.it>
2024-02-23 15:42:53 +0100

    Move spikes conversion into main script

--- Optimizer/Mnist/mnistTest.py
+++ Optimizer/Mnist/mnistTest.py
@@ -8,12 +8,75 @@ import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-from poisson import imgToSpikeTrain
 from network import run, rest, createNetwork
 from storeParameters import *
 from utils import checkBitWidth, fixedPoint
 
 from bitWidths import *
+
+def imgToSpikeTrain(image, num_steps, rng):
+
+	""" 
+	Convert a black and white image into spike trains using the Poisson
+	method.
+
+	INPUT:
+
+		1) image: NumPy array containing the value of each pixel
+		expressed as an integer.
+
+		2) dt: float. Time step duration, expressed in milliseconds. 
+
+		3) trainingSteps: total amount of time steps associated to the
+		pixels" spikes trains.
+
+		4) inputIntensity: current value of the pixel"s intensity.
+
+		5) rng: NumPy random generator.
+
+
+	OUTPUT:
+
+		Two-dimensional boolean NumPy array. Each row corresponds to a
+		time step. Each column corresponds to a pixel.
+	"""
+
+	# Create two-dimensional array of random values
+	random2D = rng.uniform(size = (num_steps, image.shape[0]))
+
+	# Convert the image into spikes trains
+	return poisson(image, random2D)
+
+
+def poisson(image, random2D):
+
+	""" 
+	Poisson convertion of the numerical values of the pixels into spike
+	trains. 
+
+	INPUT PARAMETERS:
+
+		1) image: NumPy array containing the value of each pixel
+		expressed as an integer.
+
+		2) dt: time step duration, expressed in milliseconds. 
+
+		3) random2D: teo-dimensional NumPy array containing random
+		values between pixelMin and pixelMax.
+
+		4) inputIntensity: current value of the pixel"s intensity.
+
+
+	OUTPUT:
+
+		Boolean two-dimensional array containing one spikes"
+		train for each pixel.  
+	"""
+
+	# Create the boolean array of spikes with Poisson distribution
+	return image[:] > random2D
+
+
 
 # Directory in which parameters and performance of the network are stored
 paramDir = "./Parameters128"
@@ -120,9 +183,6 @@ countThreshold = 5
 
 # NumPy default random generator.
 rng = np.random.default_rng()
-
-
-
 
 
 
