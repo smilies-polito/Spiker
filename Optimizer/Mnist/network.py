01697b723000a34521c3f4ad44f2e5271a19bf4e
alessio <alessio.carpegna@polito.it>
2024-02-23 15:24:26 +0100

    Move layers functions into network script
    Try to minimize the number of scripts

--- Optimizer/Mnist/network.py
+++ Optimizer/Mnist/network.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-from layers import updateExcLayer
 from synapses import stdp 
 
+from utils import expDecay, checkBitWidth, saturateArray
+from bitWidths import fixed_point_decimals, neuron_bitWidth
 
 
 def run(network, networkList, spikesTrains, dt_tauDict, exp_shift, stdpDict,
@@ -154,4 +155,115 @@ def rest(network, networkList):
 		# Reset the membrane potential to the rest value
 		network["excLayer" + str(layer)]["v"][0][:] = network["excLayer"
 			+ str(layer)]["vRest"]
+
+
+
+def updateExcLayer(network, layer, exp_shift, inputSpikes, neuron_bitWidth):
+
+	"""
+	One training step update of the excitatory layer.
+
+	INPUT:
+	
+		1) network: dictionary of the network.
+
+		2) layer: index of the current layer. The count starts from 1.
+
+		3) exp_shift: bit shift for the exponential decay.
+		
+		4) inputSpikes: boolean NumPy array containing the input spikes.
+
+		5) neuron_bitWidth: number of bits on which the neuron works.
+
+	"""
+
+	layerName = "excLayer" + str(layer)
+	
+	# Generate spikes if the potential exceeds the dynamic threshold	
+	generateOutputSpikes(network, layerName)
+
+	# Reset the potential of the active neurons	
+	resetPotentials(network, layerName)
+
+	# Exponentially decrease the membrane potential
+	expDecay(network, layerName, exp_shift, "v")
+
+
+	if np.sum(inputSpikes) != 0:
+
+		# Update membrane potential with the spikes from the previous layer
+		all2allUpdate(network, layerName, "exc2exc" + str(layer),
+				inputSpikes, neuron_bitWidth)
+
+
+	checkBitWidth(network["excLayer" + str(layer)]["v"],
+			neuron_bitWidth)
+
+
+def generateOutputSpikes(network, layerName): 
+
+	""" 
+	Generate the output spikes for all those neurons whose membrane
+	potential exceeds the threshold (variable with the homeostasis).
+
+	INPUT: 
+
+		1) network: dictionary of the network.
+
+		2) layerName: string. Complete name of the layer, including the
+		index of the layer itself.
+	"""
+	
+	network[layerName]["outSpikes"][0] = network[layerName]["v"][0] > \
+		network[layerName]["vThresh"]
+
+
+
+
+def resetPotentials(network, layerName):
+
+	""" 
+	Reset the membrane potential of all those neurons whose membrane
+	potential has exceeded the threshold. 
+
+	INPUT:
+		1) network: dictionary of the network.
+
+		2) layerName: string. Complete name of the layer, including the
+		index of the layer itself.
+
+	"""
+
+
+	network[layerName]["v"][0][network[layerName]["outSpikes"][0]] = \
+		network[layerName]["vReset"]
+
+
+
+
+def all2allUpdate(network, layerName, synapseName, inputSpikes, bitwidth):
+
+	""" 
+	Update the membrane potential of a fully connected layer.
+
+	INPUT:
+		
+		1) network: dictionary of the network.
+
+		2) layerName: string. Complete name of the layer, including the
+		index of the layer itself.
+
+		3) synapseName: string. Complete name of the synapse, including the
+		index of the synapse itself.
+
+		4) inputSpikes: boolean NumPy array containing the input spikes.
+
+	"""
+
+	network[layerName]["v"][0] = network[layerName]["v"][0] + np.sum(
+				network[synapseName]["weights"][:, inputSpikes],
+				axis=1)
+
+	network[layerName]["v"][0] = saturateArray(network[layerName]["v"][0],
+			bitwidth)
 
