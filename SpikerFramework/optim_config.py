6ff7cab0cf0ba11334e7010f78be2908cc1edef9
alessio <alessio.carpegna@polito.it>
2024-09-18 15:42:56 +0200

    First draft of optimizer configuration dictionary

---
+++ SpikerFramework/optim_config.py
@@ -0,0 +1,28 @@
+optim_config = {
+
+	"layer_0"	: {
+
+		"weights_bw"	: {
+			"min"	: 4,
+			"max"	: 8
+		},
+
+		"neruons_bw"	: {
+			"min"	: 4,
+			"max"	: 10
+		}
+	},
+
+	"layer_1"	: {
+
+		"weights_bw"	: {
+			"min"	: 4,
+			"max"	: 8
+		},
+
+		"neruons_bw"	: {
+			"min"	: 4,
+			"max"	: 10
+		}
+	}
+}
