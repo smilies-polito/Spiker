f03cc087e859989ff928f2b7253661c800455757
alessio <alessio.carpegna@polito.it>
2024-09-18 10:11:32 +0200

    Configure audio mnist network

--- SpikerFramework/net_dict.py
+++ SpikerFramework/net_dict.py
@@ -1,30 +1,31 @@
 net_dict = {
 
-		"n_cycles"				: 100,
-
-		"n_inputs"				: 10,
+		"n_cycles"				: 73,
+		"n_inputs"				: 40,
 
 		"layer_0"	: {
 			
-			"neuron_model"		: "if",
-			"n_neurons"			: 4,
-			"alpha"				: 0.8,
+			"neuron_model"		: "lif",
+			"n_neurons"			: 128,
+			"alpha"				: None,
 			"learn_alpha"		: False,
-			"beta"				: 0.9,
+			"beta"				: 0.9375,
 			"learn_beta"		: False,
 			"threshold"			: 1.,
-			"learn_threshold"	: False
+			"learn_threshold"	: False,
+			"reset_mechanism"	: "subtract"
 		},
 
 		"layer_1"	: {
 			
 			"neuron_model"		: "lif",
-			"n_neurons"			: 4,
-			"alpha"				: 0.8,
+			"n_neurons"			: 10,
+			"alpha"				: None,
 			"learn_alpha"		: False,
-			"beta"				: 0.9,
+			"beta"				: 0.9375,
 			"learn_beta"		: False,
 			"threshold"			: 1.,
-			"learn_threshold"	: False
+			"learn_threshold"	: False,
+			"reset_mechanism"	: "none"
 		}
 }
