93c536cb82541a427e05cf5e5cc94d4a630cce11
alessio <alessio.carpegna@polito.it>
2024-09-18 09:22:21 +0200

    Train the network for a single epoch

--- SpikerFramework/trainer.py
+++ SpikerFramework/trainer.py
@@ -36,3 +36,32 @@ class Trainer:
 			self.device = torch.device("cpu")
 
 		self.net.to(self.device)
+
+
+	def train_one_epoch(self, dataloader):
+
+		# Iterate over the dataloader
+		for batch_idx, (data, _, labels) in enumerate(dataloader):
+
+			data 	= data.to(self.device)
+			labels	= labels.to(self.device)
+
+			self.optimizer.zero_grad()
+
+			self.net.train()
+
+			_, out_rec = net(data)
+
+			# Reshape mem_rec to combine the time and batch dims
+			out_rec_flat = out_rec.view(-1, out_rec.shape[-1])
+			labels_repeat = labels.repeat(out_rec.shape[0])
+
+			# Compute the loss over all time steps at once
+			loss_val = self.loss_fn(out_rec_flat, labels_repeat)
+
+			# Gradient calculation + weight update
+			loss_val.backward()
+			self.optimizer.step()
+
+		_, idx = out_rec.sum(dim=0).max(1)
+		accuracy = np.mean((labels == idx).detach().cpu().numpy())
