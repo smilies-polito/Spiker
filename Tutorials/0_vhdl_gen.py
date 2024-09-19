9026794f4c0dbb0d65d1d706d7e7c59d2235fc97
alessio <alessio.carpegna@polito.it>
2024-09-19 17:45:41 +0200

    First draft of a tutorial to test vhdl generator

---
+++ Tutorials/0_vhdl_gen.py
@@ -0,0 +1,53 @@
+from spiker import NetBuilder, VhdlGenerator
+
+net_dict = {
+
+		"n_cycles"				: 73,
+		"n_inputs"				: 40,
+
+		"layer_0"	: {
+			
+			"neuron_model"		: "lif",
+			"n_neurons"			: 128,
+			"alpha"				: None,
+			"learn_alpha"		: False,
+			"beta"				: 0.9375,
+			"learn_beta"		: False,
+			"threshold"			: 1.,
+			"learn_threshold"	: False,
+			"reset_mechanism"	: "subtract"
+		},
+
+		"layer_1"	: {
+			
+			"neuron_model"		: "lif",
+			"n_neurons"			: 10,
+			"alpha"				: None,
+			"learn_alpha"		: False,
+			"beta"				: 0.9375,
+			"learn_beta"		: False,
+			"threshold"			: 1.,
+			"learn_threshold"	: False,
+			"reset_mechanism"	: "none"
+		}
+}
+
+optim_config = {
+
+	"weights_bw"	: 4,
+	"neurons_bw"	: 8,
+	"fp_dec"		: 3
+
+}
+
+
+net_builder = NetBuilder(net_dict)
+
+snn = net_builder.build()
+
+vhdl_generator = VhdlGenerator(snn, optim_config)
+
+import torch
+ff_w = torch.ones((128, 40)).numpy()
+
+print(vhdl_generator.init_layer("lif1", ff_w).code())
